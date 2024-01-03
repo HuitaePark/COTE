d9dcca8af725235d4fa04bcae78b47325eb54fd9
HuitaePark <154016341+HuitaePark@users.noreply.github.com>
2024-01-03 23:33:30 +0900

    Update 9.모두의 약수.cpp

--- Infrearn_ lecture/9.모두의 약수.cpp
+++ Infrearn_ lecture/9.모두의 약수.cpp
@@ -1,1 +1,21 @@
+#include<stdio.h>
+using namespace std;
 
+int main() {
+    int a = 0;
+    scanf("%d", &a);
+    for (int i = 0; i <= a; i++)
+    {
+        for (int j = 0; j <= i; j++) {
+            if (i % j == 0) {
+                int count = 0;
+                count++;
+                printf("%d ", count);
+            }
+        }
+   
+    }
+
+    return 0;
+
+}
