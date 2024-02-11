d245b709084f775bf64f279f4a5c69e71d3a02f4
HuitaePark <154016341+HuitaePark@users.noreply.github.com>
2024-02-11 21:53:55 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
@@ -7,7 +7,7 @@ string solution(string my_string, string letter) {
     string answer = "";
     for (int i = 0; i < my_string.size(); i++) {
         if (my_string[i] != letter[0]) {
-            answer += my_string[i];
+            answer += my_string[i];//
         }
     }
     return answer;
