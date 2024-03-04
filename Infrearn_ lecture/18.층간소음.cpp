81b210e53999fe074c7cdd3bf47fa75cbd0a82a8
HuitaePark <154016341+HuitaePark@users.noreply.github.com>
2024-03-04 23:02:00 +0900

    Update 18.층간소음.cpp

--- Infrearn_ lecture/18.층간소음.cpp
+++ Infrearn_ lecture/18.층간소음.cpp
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
 int main(){
-    int s=0,n,m,cnt=0;
+    int high=0,n,m,cnt=0;
     int second[1001];
     scanf("%d",&n);
     scanf("%d",&m);
     for(int i = 0;i<n;i++){
         scanf("%d",&second[i]);
     }
-    for (int i = 0; i!='\0'; i++)
+    for (int i = 0; second[i]!='\0'; i++)
     {
-        if(second[i-1]>m && second[i]>m){
+        if(second[i]>m){
             cnt++;
+            if(cnt>high){
+                high=cnt;
+            }
+        }
+        else{
+            cnt = 0;
         }
     }
     
-    
-    printf("%d",cnt);
-    
+    printf("%d",high);
     
     
     return 0;
