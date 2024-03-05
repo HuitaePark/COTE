58e993f917ddbfc992d67b2874c80d18c7239d79
HuitaePark <154016341+HuitaePark@users.noreply.github.com>
2024-03-05 15:59:03 +0900

    Update 17.선생님_퀴즈.cpp

--- Infrearn_ lecture/17.선생님_퀴즈.cpp
+++ Infrearn_ lecture/17.선생님_퀴즈.cpp
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main(){
-   int cnt = 0;
-
+    int num,sum=0,solve,cnt = 0;
    
    scanf("%d",&cnt);
    
    for (int i = 0; i < cnt; i++)
    {
-    int num, solve, sum = 0;
         
         scanf("%d",&num);
         scanf("%d",&solve);
+        sum=0;
         for (int j = 1; j <= num; j++)
         {
+            
             sum+=j;
         }
         if (sum==solve)
