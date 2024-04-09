ef4af287f092f5dc3a101c3baf5b1c3df0e7037d
HuitaePark <154016341+HuitaePark@users.noreply.github.com>
2024-04-09 18:01:44 +0900

    [level 2] Title: 올바른 괄호, Time: 0.67 ms, Memory: 3.81 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool solution(string s)
+{
+    stack<int> st;
+    int openCount = 0;  // '('의 개수
+    int closeCount = 0; // ')'의 개수
+    
+    // s 문자열 순회
+    for(int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '(')
+        {
+            st.push(s[i]);
+            openCount++;
+        }
+        
+        if (s[i] == ')')
+        {
+            closeCount++;
+            
+            if (!st.empty())
+                st.pop();
+        }
+    }
+    
+    // s 문자열 순회 끝
+    if (st.empty() && openCount == closeCount && openCount > 0 && closeCount > 0)
+        return true;
+    else
+        return false;
+}
