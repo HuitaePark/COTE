eda368cedc25be7ecba87f7271ea6b6897d04244
HuitaePark <qkrgmlxo3174@gmail.com>
2024-08-29 10:06:18 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 168 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt()-543;
+        
+        System.out.println(year);
+    }
+}
