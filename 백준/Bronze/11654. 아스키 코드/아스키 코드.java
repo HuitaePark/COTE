20591d48cd608fda4096ca4f4a6a53323d829c15
HuitaePark <qkrgmlxo3174@gmail.com>
2024-09-03 11:00:49 +0900

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+		String n = sc.next();
+		char c = n.charAt(0);
+
+		System.out.println((int)c);
+		sc.close();
+    }
+}
