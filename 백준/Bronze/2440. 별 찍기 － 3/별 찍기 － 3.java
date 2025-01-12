936d414ba7512986932ae7039df3a6e894d4be56
Baki <qkrgmlxo3174@gmail.com>
2025-01-13 01:08:14 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 입력을 효율적으로 읽어오기 위해 BufferedReader 사용
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringBuilder는 결과를 빠르게 저장하고 출력하기 위해 사용
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        while (n>0){
+            sb.append("*".repeat(n));
+            sb.append("\n");
+            n--;
+        }
+        System.out.println(sb);
+    }
+}
