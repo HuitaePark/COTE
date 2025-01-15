b6615c326c2a407ca2359b3bd9f3b75802a2491c
Baki <qkrgmlxo3174@gmail.com>
2025-01-15 18:06:42 +0900

    [Bronze V] Title: 개수 세기, Time: 112 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        int count = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        int answer = Integer.parseInt(st.nextToken());
+        for (int j : arr) {
+            if (j == answer) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
