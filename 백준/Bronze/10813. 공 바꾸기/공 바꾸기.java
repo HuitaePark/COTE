d190f9b642a6d546b104aaa83ab383812fb3bb18
Baki <qkrgmlxo3174@gmail.com>
2025-01-16 16:26:10 +0900

    [Bronze II] Title: 공 바꾸기, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = i+1;
+        }
+        int tmp = 0;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken())-1;
+            int b = Integer.parseInt(st.nextToken())-1;
+
+            tmp = arr[a];
+            arr[a]=arr[b];
+            arr[b]=tmp;
+        }
+        for (int num : arr) {
+            sb.append(num).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
