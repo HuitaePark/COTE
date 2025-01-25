98e4d3865d36bf169f1266eecef0220ae5736ef6
Baki <qkrgmlxo3174@gmail.com>
2025-01-25 17:30:26 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int m = 1;
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <=n; i++) {
+            sb.append(" ".repeat(n-i)).append("*".repeat(i+i-1)).append("\n");
+        }
+        for (int i = n-1; i >=1 ; i--) {
+            sb.append(" ".repeat(m)).append("*".repeat(i+i-1)).append("\n");
+            m++;
+        }
+        System.out.println(sb);
+    }
+}
