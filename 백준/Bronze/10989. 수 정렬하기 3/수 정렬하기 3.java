410b5370cce0b9ba859cd96098b3feb4143bd0f7
Baki <qkrgmlxo3174@gmail.com>
2025-01-27 16:06:26 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2488 ms, Memory: 346836 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Arrays;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for (int i : arr) {
+            bw.write(i + "\n");
+        }
+        bw.flush();
+    }
+}
