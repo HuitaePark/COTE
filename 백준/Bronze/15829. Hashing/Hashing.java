184defdd0e22d83f5f8ed91dfab6a7d153711070
Baki <qkrgmlxo3174@gmail.com>
2025-02-02 18:37:14 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14304 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,19 +1,22 @@
 import java.io.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
-        int sum = 0;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        long sum = 0;
         int n = Integer.parseInt(br.readLine());
         String str = br.readLine();
+        long power = 1;
+        int p = 31;
+        long mod = 1234567891; // 모듈러 값
 
-        for(int i=0;i<n;i++){
-            sum += (str.charAt(i)-'a'+1) * (int)Math.pow(31,i);
+        for (int i = 0; i < n; i++) {
+            int charValue = str.charAt(i) - 'a' + 1;
+            sum = (sum + charValue * power) % mod;
+            power = (power * p) % mod;
         }
-        bw.write(Integer.toString(sum));
+        bw.write(Long.toString(sum));
         bw.flush();
     }
-
 }
