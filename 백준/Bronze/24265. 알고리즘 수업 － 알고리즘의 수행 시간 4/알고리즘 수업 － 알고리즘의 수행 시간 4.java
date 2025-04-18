bbdca240a5604379ba662c15e5f87baa473b7369
Baki <qkrgmlxo3174@gmail.com>
2025-04-18 16:20:14 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    
+    long n = Long.parseLong(br.readLine());
+    System.out.println(n*(n-1)/2);
+    System.out.println(2);
+    
+
+
+
+  }
+}
