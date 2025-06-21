3d0b4c757a9f83b927be2f1d71f31e17a0354b8c
Baki <qkrgmlxo3174@gmail.com>
2025-06-21 21:04:17 +0900

    [Bronze IV] Title: 베라의 패션, Time: 96 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+
+    
+
+
+    sb.append(n*(n-1));
+
+
+     bw.write(sb.toString());
+     bw.flush();
+  
+  }
+}
