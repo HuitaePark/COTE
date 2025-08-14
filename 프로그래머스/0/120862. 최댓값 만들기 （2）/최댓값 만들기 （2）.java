7fd98d7caa064ba1c7863e0c78a5c09d12e1cabf
Baki <qkrgmlxo3174@gmail.com>
2025-08-14 11:09:52 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 1.16 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        return Math.max(numbers[0]*numbers[1],numbers[numbers.length-1]*numbers[numbers.length-2]);
+    }
+}
