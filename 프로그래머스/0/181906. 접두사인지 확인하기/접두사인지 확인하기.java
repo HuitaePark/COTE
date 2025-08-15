0fbae0c307aca165ad8aea7c04a0acea07ccae38
Baki <qkrgmlxo3174@gmail.com>
2025-08-15 19:13:31 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        if(my_string.length()<is_prefix.length()) return 0;
-        return my_string.substring(0,is_prefix.length()).equals(is_prefix) ? 1 : 0;
+       return my_string.startsWith(is_prefix) ? 1 : 0;
     }
 }
