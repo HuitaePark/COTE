2efc3c2b8e49262897703f52e2bf50b92c6e8bfe
Baki <qkrgmlxo3174@gmail.com>
2025-08-16 03:09:21 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 2.58 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int[] num_list) {
+        return IntStream.range(0,num_list.length).filter(i -> num_list[i]<0).findFirst().orElse(-1);
+    }
+}
