b7694745145cfc6edda2efcfcb9663a4fc3c0eec
Baki <qkrgmlxo3174@gmail.com>
2025-08-19 00:04:10 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 2.91 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        list.add(n);
+        
+        while(n!=1){
+            if(n%2==0){
+                n = n/2;
+            }
+            else{
+                n = (n*3+1);
+            }
+            list.add(n);
+        }
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
