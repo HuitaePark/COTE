1188ca579e2b9faa0e668a4d6e8ed30b53607eca
Baki <qkrgmlxo3174@gmail.com>
2025-08-19 10:05:23 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.08 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        return a != b && b!=c && a!=c ? a+b+c : 
-        a==b && b==c ? (a+b+c)*(int)(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2))*(int)(Math.pow(a,3)+Math.pow(b,3)+Math.pow(c,3)) : (a+b+c)*(int)(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2));
+        return a != b && b!=c && a!=c ? a+b+c : a==b && b==c ? (a+b+c)*(int)(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2))*(int)(Math.pow(a,3)+Math.pow(b,3)+Math.pow(c,3)) : (a+b+c)*(int)(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2));
     }
 }
