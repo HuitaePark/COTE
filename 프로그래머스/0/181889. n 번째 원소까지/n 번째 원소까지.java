6c4341f053e70e839d00e5257b7f1e55df97100b
Baki <qkrgmlxo3174@gmail.com>
2025-10-18 17:07:30 +0900

    [level 0] Title: n 번째 원소까지, Time: 4.91 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,7 @@
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        return Arrays.stream(num_list).limit(n).toArray();
+    }
+}
