04a85406abcfb60db4c2d3c24b4a3c974a268c29
Baki <qkrgmlxo3174@gmail.com>
2025-12-01 23:01:13 +0900

    Time: 2 ms (98.72%), Space: 43.9 MB (69.31%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,8 +1,26 @@
 import java.util.*;
 class Solution {
     public boolean isPalindrome(String s) {
-        String ss = s.replaceAll("[^A-Za-z0-9]","").toLowerCase();
-        String reversed = new StringBuilder(ss).reverse().toString();
-        return ss.equals(reversed) ? true : false;
+        int start = 0;
+        int end = s.length()-1;
+
+        while(start<end){
+            if(!Character.isLetterOrDigit(s.charAt(start))){
+                start++;
+            }
+            else if(!Character.isLetterOrDigit(s.charAt(end))){
+                end--;
+            }
+            else{
+                if(Character.toLowerCase(s.charAt(start))!=Character.toLowerCase(s.charAt(end))){
+                    return false;
+                }
+                else{
+                    start++;
+                    end--;
+                }
+            }
+        }
+        return true;
     }
 }
