1f2caced71c7da1c4520233810bb78e97c380eba
Baki <qkrgmlxo3174@gmail.com>
2025-12-02 12:43:26 +0900

    [level 2] Title: 행렬의 곱셈, Time: 8.43 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -4,14 +4,15 @@ class Solution {
         int c1 = arr1[0].length;
         int r2 = arr2.length;
         int c2 = arr2[0].length;
+        
         int[][] answer = new int[r1][c2];
         
-        for(int i = 0;i<r1;i++){
+        for(int i =0;i<r1;i++){
             for(int j = 0; j<c2;j++){
                 for(int k =0;k<c1;k++){
-                    answer[i][j] += arr1[i][k] * arr2[k][j]; 
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
                 }
-            }    
+            }
         }
             
         return answer;
