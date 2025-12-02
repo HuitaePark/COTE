7f6793ac36db25f4d8ef06960880aad1e02ea044
Baki <qkrgmlxo3174@gmail.com>
2025-12-02 17:24:48 +0900

    [Silver II] Title: 스택 수열, Time: 312 ms, Memory: 27344 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,44 +1,50 @@
-import java.util.Scanner;
-import java.util.Stack;
- 
-public class Main {
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();	// 출력할 결과물 저장
-		
-		Stack<Integer> stack = new Stack<>();
-		
-		int N = in.nextInt();
-		
-		int start = 0;
-		
-		// N 번 반복
-		while(N -- > 0) {
-			
-			int value = in.nextInt();
-			
-			if(value > start) {
-				// start + 1부터 입력받은 value 까지 push를 한다.
-				for(int i = start + 1; i <= value; i++) {
-					stack.push(i);
-					sb.append('+').append('\n');	// + 를 저장한다. 
-				}
-				start = value; 	// 다음 push 할 때의 오름차순을 유지하기 위한 변수 초기화 
-			}
-			
-			// top에 있는 원소가 입력받은 값과 같이 않은 경우  
-			else if(stack.peek() != value) {
-				System.out.println("NO");
-				return;		// 또는 System.exit(0); 으로 대체해도 됨. 
-			}
-			
-			stack.pop();
-			sb.append('-').append('\n');
-			
-		}
-		
-		System.out.println(sb);
-	}
+import java.util.*;
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    
+    int n = Integer.parseInt(br.readLine());
+    Stack<Integer> stack = new Stack<>();
+    int[] arr = new int[n + 1];
+    
+    for (int i = 1; i <= n; i++) {
+      arr[i] = Integer.parseInt(br.readLine());
+    }
+
+    int num = 1;
+
+    for (int i = 1; i <= n; i++) {
+      int current = arr[i];
+
+      if (current >= num) {
+    
+        while (current >= num) {
+          stack.push(num++);
+          sb.append("+\n");
+        }
+
+        stack.pop();
+        sb.append("-\n");
+      } 
+      else {
+        int m = stack.pop();
+
+        if (m > current) {
+          bw.write("NO\n");
+          bw.flush();
+          return;
+          
+        } else {
+          sb.append("-\n");
+        }
+      }
+    }
+
+    bw.write(sb.toString());
+    bw.flush();
+  }
 }
- 
