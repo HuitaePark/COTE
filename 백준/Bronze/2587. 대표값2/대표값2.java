8bc1d8c8e25092b878bd1f7fe3eb7c359dc2620d
Baki <qkrgmlxo3174@gmail.com>
2025-12-17 21:37:31 +0900

    [Bronze II] Title: 대표값2, Time: 100 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.java
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -1,18 +1,25 @@
 import java.util.*;
 import java.io.*;
-
+  
 public class Main{
   public static void main(String args[]) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    int[] arr = new int[5];
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
     int sum = 0;
+    int[] arr = new int[5]; 
     for(int i = 0;i<5;i++){
-      arr[i] = Integer.parseInt(br.readLine());
-      sum += arr[i];
+      int current = Integer.parseInt(br.readLine());
+      arr[i] = current;
+      sum+=current;
     }
+
     Arrays.sort(arr);
-    bw.write(Integer.toString(sum/5)+"\n"+Integer.toString(arr[2]));
+
+    sb.append(sum/5).append("\n").append(arr[2]);
+
+    bw.write(sb.toString());
     bw.flush();
+  
   }
 }
