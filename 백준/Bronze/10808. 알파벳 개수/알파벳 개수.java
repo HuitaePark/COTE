4b94414179a83c771178482517a8c1a956c7ac25
Baki <qkrgmlxo3174@gmail.com>
2025-12-17 17:22:54 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,20 +1,24 @@
 import java.util.*;
 import java.io.*;
-
+  
 public class Main{
   public static void main(String args[]) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    String str = br.readLine();
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    String s = br.readLine();
     int[] arr = new int[26];
     
-    for(int i = 0;i<str.length();i++){
-      int n = str.charAt(i)-'a';
-      arr[n]++;
-     }
+    for(int i = 0;i<s.length();i++){
+      arr[s.charAt(i)-'a']++;
+    }
+
     for(int i : arr){
-      bw.write(i + " ");
-      }
+      sb.append(i).append(" ");
+    }
+
+    bw.write(sb.toString());
     bw.flush();
+  
   }
 }
