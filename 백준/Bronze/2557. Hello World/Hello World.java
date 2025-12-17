148916ed98b1c9eacaf97dccace4f22f91ad9b4a
Baki <qkrgmlxo3174@gmail.com>
2025-12-17 17:36:01 +0900

    [Bronze V] Title: Hello World, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+import java.util.*;
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+
+    sb.append("Hello World!");
+
+    bw.write(sb.toString());
+    bw.flush();
+  
+  }
 }
