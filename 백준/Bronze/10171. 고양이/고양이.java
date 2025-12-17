0bbd2dadced02dcd76c0a6224f499f528702b3b3
Baki <qkrgmlxo3174@gmail.com>
2025-12-17 17:38:35 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13984 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,10 +1,23 @@
-public class Main {
-	public static void main(String[] args) {
-    
-		System.out.println("\\    /\\");
-		System.out.println(" )  ( ')");
-		System.out.println("(  /  )");
-		System.out.println(" \\(__)|");    
- 
-	}
+import java.util.*;
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+
+    sb.append("\\    /\\").append("\n");
+    sb.append(" )  ( \')").append("\n");
+    sb.append("(  /  )").append("\n");
+    sb.append(" \\(__)|").append("\n");
+
+
+
+
+
+    bw.write(sb.toString());
+    bw.flush();
+  
+  }
 }
