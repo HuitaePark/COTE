0e28ecf4a3dbc5abdf7894c0c0f3eaeb2fe23098
Baki <qkrgmlxo3174@gmail.com>
2025-12-17 21:25:37 +0900

    [Bronze III] Title: 윷놀이, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.java
+++ 백준/Bronze/2490. 윷놀이/윷놀이.java
@@ -1,48 +1,47 @@
 import java.util.*;
 import java.io.*;
-
+  
 public class Main{
   public static void main(String args[]) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+
     for(int i = 0;i<3;i++){
       StringTokenizer st = new StringTokenizer(br.readLine());
-      int[] arr = new int[4];
+      int zero = 0;
+      int one = 0;
       
       for(int j = 0;j<4;j++){
-        arr[j] = Integer.parseInt(st.nextToken());
+        int idx = Integer.parseInt(st.nextToken());
+        if(idx == 1) one ++;
+        else zero++;
       }
       
-      int count = 0;
-      
-      for(int j = 0;j<4;j++){
-        if (arr[j]==1)
-          count++;
-      }
-      
-      
-      if(count == 0){
-        bw.write("D"+"\n");
-      }
-      else if(count == 1){
-         bw.write("C"+"\n");
-      }
-      else if(count == 2){
-         bw.write("B"+"\n");
-      }
-      else if(count == 3){
-         bw.write("A"+"\n");
-      }
-      else if(count == 4){
-         bw.write("E"+"\n");
-      }
+      switch(one){
+        case 1:
+          sb.append("C").append("\n");
+          break;
+        case 2:
+          sb.append("B").append("\n");
+          break;
+        case 3:
+          sb.append("A").append("\n");
+          break;
+        case 4:
+          sb.append("E").append("\n");
+          break;
+        case 0:
+          sb.append("D").append("\n");
+          break;
+      }      
     }
-  
 
-     bw.flush();  
+
+
+
+    bw.write(sb.toString());
+    bw.flush();
   
-  
-    
   }
 }
