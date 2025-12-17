7de03a5a96a9271ddac6d8f337d24b5e4a2d6ae5
Baki <qkrgmlxo3174@gmail.com>
2025-12-18 02:47:53 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 104 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.java
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.java
@@ -1,33 +1,42 @@
 import java.util.*;
 import java.io.*;
-
+  
 public class Main{
   public static void main(String args[]) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
     int n = Integer.parseInt(br.readLine());
     int[] arr = new int[n];
+    int y = 0;
+    int m = 0;
+
     StringTokenizer st = new StringTokenizer(br.readLine());
-    for(int i=0;i<n;i++){
+    for(int i =0; i<n;i++){
       arr[i] = Integer.parseInt(st.nextToken());
     }
-    int Y = 0;
-    int M = 0;
-    
-    for(int i : arr){
-        Y += (i/30+1)*10;
-        M += (i/60+1)*15;
-      }
-     if(Y==M){
-       bw.write("Y M "+Integer.toString(M));
-     }
-     else if(Y>M){
-       bw.write("M "+Integer.toString(M));
-     }
-     else{
-       bw.write("Y "+Integer.toString(Y));
-     }
-    
+
+    for(int i = 0; i<n; i++){
+      int current = arr[i];
+      y+=(current/30)+1;
+      m+=(current/60)+1;       
+    }
+
+    int ySum = y*10;
+    int mSum = m*15;
+
+    if(ySum==mSum){
+      sb.append("Y ").append("M ").append(ySum);
+    }
+    else if(ySum<mSum){
+      sb.append("Y ").append(ySum);
+    }
+    else{
+      sb.append("M ").append(mSum);
+    }
+
+    bw.write(sb.toString());
     bw.flush();
+  
   }
 }
