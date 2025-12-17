d6d1f2be6fc642e273c24cf9747894b3ceb8c98e
Baki <qkrgmlxo3174@gmail.com>
2025-12-17 20:02:18 +0900

    [Bronze V] Title: 윤년, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,18 +1,20 @@
 import java.util.*;
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        if((n%4==0)&&(n%100!=0)){
-            System.out.println("1");
-        }
-        else if((n%4==0)&&(n%400==0)){
-            System.out.println("1");
-        }
-        else{
-             System.out.println("0");
-        }
-        sc.close();
-    }
+    int year = Integer.parseInt(br.readLine());
+
+    if(year%4==0 && (year%100!=0 || year%400==0)) sb.append(1);
+    else sb.append(0);
+
+
+    bw.write(sb.toString());
+    bw.flush();
+  
+  }
 }
