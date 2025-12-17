9ce689e9292ac66c6407ea6d5b11d0a834c79e19
Baki <qkrgmlxo3174@gmail.com>
2025-12-17 18:38:49 +0900

    [Bronze IV] Title: 세수정렬, Time: 108 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.java
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -1,21 +1,25 @@
 import java.util.*;
 import java.io.*;
-
+  
 public class Main{
   public static void main(String args[]) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    StringTokenizer st= new StringTokenizer(br.readLine());
     int[] arr = new int[3];
-    StringTokenizer st  = new StringTokenizer(br.readLine());
     for(int i =0;i<3;i++){
       arr[i] = Integer.parseInt(st.nextToken());
     }
+
     Arrays.sort(arr);
+
     for(int i : arr){
-      bw.write(Integer.toString(i)+" ");
+      sb.append(i).append(" ");
     }
 
-    
+    bw.write(sb.toString());
     bw.flush();
+  
   }
 }
