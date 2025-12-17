bd8823784aebcf7524e1ea13f77b226b7f73a3f7
Baki <qkrgmlxo3174@gmail.com>
2025-12-17 18:25:32 +0900

    [Bronze V] Title: 시험 성적, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,25 +1,25 @@
 import java.util.*;
-
+import java.io.*;
+  
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-		int score = sc.nextInt();		
-        
-        if(score>=90){
-            System.out.println("A");
-        }
-        else if(score>=80){
-            System.out.println("B");
-        }
-        else if(score>=70){
-            System.out.println("C");
-        }
-        else if(score>=60){
-            System.out.println("D");
-        }
-        else{
-            System.out.println("F");
-        }
-        sc.close();
-    }
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+
+    if(n>=90) sb.append("A");
+    else if(n<90 && n>=80) sb.append("B");
+    else if(n<80 && n>=70) sb.append("C");
+    else if(n<70 && n>=60) sb.append("D");
+    else sb.append("F");
+
+
+
+
+
+    bw.write(sb.toString());
+    bw.flush();
+  
+  }
 }
