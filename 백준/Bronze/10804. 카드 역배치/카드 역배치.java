3eb95ec321b5bbc9339b72cee668d6d8a1289a67
Baki <qkrgmlxo3174@gmail.com>
2025-12-18 13:12:26 +0900

    [Bronze II] Title: 카드 역배치, Time: 108 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Bronze/10804. 카드 역배치/카드 역배치.java
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.java
@@ -1,39 +1,39 @@
 import java.util.*;
 import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    StringTokenizer st;
+    int[] arr = new int[21];
 
-public class Main {
-    public static void reverseSubArray(int[] arr, int start, int end) {
-        while (start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
+    for(int i = 1;i<=20;i++){
+      arr[i]= i;
     }
 
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[] arr = new int[20];
+    for(int i =0;i<10;i++){
+      st = new StringTokenizer(br.readLine());
+      int start = Integer.parseInt(st.nextToken());
+      int end = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < 20; i++) {
-            arr[i] = i + 1;  
-        }
-
-        for (int i = 0; i < 10; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken()) - 1; 
-            int b = Integer.parseInt(st.nextToken()) - 1; 
-
-            reverseSubArray(arr, a, b);
-        }
-
-        for (int i : arr) {
-            bw.write(i + " ");
-        }
-
-        bw.flush();
+      while(start<end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+      }
     }
+    
+    for(int i : arr){
+      if(i == 0) continue;
+      sb.append(i).append(" ");
+    }
+
+    bw.write(sb.toString());
+    bw.flush();
+  
+  }
 }
-
