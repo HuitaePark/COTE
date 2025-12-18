0a808c679b6f7905388da84396e3af9c90811dac
Baki <qkrgmlxo3174@gmail.com>
2025-12-18 13:23:25 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -1,20 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
-public class Main {
-    public static void main(String[] args) throws IOException {
-        // 입력을 효율적으로 읽어오기 위해 BufferedReader 사용
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // StringBuilder는 결과를 빠르게 저장하고 출력하기 위해 사용
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        for (int i = n; i > 0; i--) {
-            sb.append(" ".repeat(n - i))  // 앞의 공백
-                    .append("*".repeat(i))      // 별 출력
-                    .append("\n");             // 줄바꿈
-        }
-        System.out.print(sb);
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+    for(int i = n;i>0;i--){
+      sb.append(" ".repeat(n-i)).append("*".repeat(i)).append("\n");
     }
+
+
+    bw.write(sb.toString());
+    bw.flush();
+  
+  }
 }
