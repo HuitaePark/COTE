81b54a0c8f09007800f20d63cbc6e89ac8996012
Baki <qkrgmlxo3174@gmail.com>
2025-12-18 13:39:36 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -1,19 +1,24 @@
+import java.util.*;
 import java.io.*;
-
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int m = 1;
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 1; i <=n; i++) {
-            sb.append(" ".repeat(n-i)).append("*".repeat(i+i-1)).append("\n");
-        }
-        for (int i = n-1; i >=1 ; i--) {
-            sb.append(" ".repeat(m)).append("*".repeat(i+i-1)).append("\n");
-            m++;
-        }
-        System.out.println(sb);
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    
+    int n = Integer.parseInt(br.readLine());
+    int current = n;
+    for(int i = 1;i<=n;i++){
+      sb.append(" ".repeat(n-i)).append("*".repeat(2*i-1)).append("\n");
+      current--;
     }
+    for(int i = n-1;i>0;i--){
+      sb.append(" ".repeat(n-i)).append("*".repeat(2*i-1)).append("\n");
+    }
+
+     bw.write(sb.toString());
+     bw.flush();
+  
+  }
 }
