8e93cca4c568c850ebb22bcfaa0f14f96b2023db
Baki <qkrgmlxo3174@gmail.com>
2025-12-18 13:20:24 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+    for(int i = 1;i<=n;i++){
+      sb.append(" ".repeat(n-i)).append("*".repeat(i)).append("\n");
+    }
+
+    bw.write(sb.toString());
+    bw.flush();
+  
+  }
+}
