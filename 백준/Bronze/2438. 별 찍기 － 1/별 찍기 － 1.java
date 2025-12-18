37487a5f6ac68030330f18bbba8c60e346676bd2
Baki <qkrgmlxo3174@gmail.com>
2025-12-18 13:16:53 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,24 +1,18 @@
-import java.util.Scanner;
-public class Main {
+import java.util.*;
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+    for(int i = 1;i<=n;i++){
+      sb.append("*".repeat(i)).append("\n");
+    }
 
-	public static void main(String[] args) {
-
-
-
-
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-
-		for(int i = 0;i<n;i++){
-			for(int j=0;j<=i;j++){
-				System.out.print("*");
-			}
-			System.out.println(" ");
-		}
-
-		sc.close();
-
-
-
-			}
+    bw.write(sb.toString());
+    bw.flush();
+  
+  }
 }
