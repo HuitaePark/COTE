e2b9209ac6bb108fd87db5f708a38c9138c678fd
Baki <qkrgmlxo3174@gmail.com>
2025-12-18 13:58:20 +0900

    [Bronze III] Title: 최댓값, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,22 +1,22 @@
 import java.util.*;
-
+import java.io.*;
+  
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-	    int[] numbers = new int[9];
-        int max = 0;
-        int index = 0;
-        for (int i = 0; i < 9; i++) {
-            numbers[i] = sc.nextInt();
-        }
-        for(int i = 0; i<numbers.length;i++){
-            if(numbers[i]>max){
-                max = numbers[i];
-                index = i;
-            }
-        }
-        System.out.println(max);
-        System.out.println(index+1);
-        sc.close();
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    Map<Integer,Integer> map = new HashMap<>();
+    for(int i = 0;i<9;i++){
+      map.put(Integer.parseInt(br.readLine()),i);
     }
+    int max = 0;
+    for(Integer key : map.keySet()){
+      if(key>max) max = key;
+    }
+    sb.append(max).append("\n").append(map.get(max)+1);
+    bw.write(sb.toString());
+    bw.flush();
+  
+  }
 }
