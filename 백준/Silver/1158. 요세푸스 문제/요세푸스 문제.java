f036c6cb79fa92673ecb78a0bbd35917c0a7062c
Baki <qkrgmlxo3174@gmail.com>
2025-12-22 11:51:27 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 152 ms, Memory: 15124 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,25 +1,35 @@
 import java.util.*;
 import java.io.*;
-
+  
 public class Main{
   public static void main(String args[]) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-     StringTokenizer st = new StringTokenizer(br.readLine());
-     Queue<Integer> q = new LinkedList<>();
-  	 int N = Integer.parseInt(st.nextToken());
-	 int K = Integer.parseInt(st.nextToken());
-    for(int i = 1; i <= N; i++) {
-			q.add(i);
-		} 
-    bw.write("<");
-     while(q.size()>1){
-       for(int i=0;i<K-1;i++){
-         q.offer(q.poll());
-       }
-      bw.write(Integer.toString(q.poll())+", ");
-     }
-    bw.write(Integer.toString(q.poll())+">");
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int k = Integer.parseInt(st.nextToken());
+    LinkedList<Integer> list = new LinkedList<>();
+    int index = 0;
+    for(int i = 1;i<=n;i++){
+      list.add(i);
+    }
+    //1 2 3 4 5 6 7
+    
+    sb.append("<");
+    List<String> strList = new ArrayList<>();
+    
+    while(!list.isEmpty()){
+      int current = (index+k-1)%list.size();
+      strList.add(Integer.toString(list.get(current))); 
+      list.remove(current);
+      index = current;
+    }
+    
+    sb.append(String.join(", ",strList));
+    sb.append(">");
+    bw.write(sb.toString());
     bw.flush();
+  
   }
 }
