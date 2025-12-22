205e653cab0eefaa2152d2c356133278d7fbd534
Baki <qkrgmlxo3174@gmail.com>
2025-12-22 11:02:33 +0900

    [Silver II] Title: 키로거, Time: 788 ms, Memory: 201304 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -1,50 +1,47 @@
-import java.io.*;
 import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int testCases = Integer.parseInt(br.readLine());
-        StringBuilder result = new StringBuilder();
-
-        while(testCases-- > 0) {
-            String input = br.readLine();
-            LinkedList<Character> list = new LinkedList<>();
-            ListIterator<Character> iterator = list.listIterator();
-            
-            for (int i = 0; i < input.length(); i++) {
-                char c = input.charAt(i);
-                switch(c) {
-                    case '<':
-                        if(iterator.hasPrevious()) {
-                            iterator.previous();
-                        }
-                        break;
-                    case '>':
-                        if(iterator.hasNext()) {
-                            iterator.next();
-                        }
-                        break;
-                    case '-':
-                        if(iterator.hasPrevious()) {
-                            iterator.previous();
-                            iterator.remove();
-                        }
-                        break;
-                    default:
-                        iterator.add(c);
-                        break;
-                }
-            }
-            
-            for(char ch : list) {
-                bw.write(ch);
-               
-            }
-             bw.write("\n");
-        }
-        
-        bw.flush();
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+    
+    for(int i = 0;i<n;i++){
+      char[] charArr = br.readLine().toCharArray();
+      LinkedList<Character> list = new LinkedList<>();
+      ListIterator<Character> it = list.listIterator();
+      
+      for(int j = 0;j<charArr.length;j++){  
+        switch(charArr[j]){
+            case '<':
+              if(it.hasPrevious())
+                it.previous();
+              break;
+            case '>':
+              if(it.hasNext())
+                it.next();
+              break;
+            case '-':
+              if(it.hasPrevious()){
+                it.previous();
+                it.remove();
+              }
+              break;
+            default:
+              it.add(charArr[j]);
+              break;
+          }
+      }
+      for(Character ch : list){
+        sb.append(ch);
+      }
+      sb.append("\n");
     }
+    
+    bw.write(sb.toString());
+    bw.flush();
+  
+  }
 }
