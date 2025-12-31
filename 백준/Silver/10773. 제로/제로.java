82b8e0dc6a0594676883d54d61119a5fc5a85039
Baki <qkrgmlxo3174@gmail.com>
2025-12-31 13:08:18 +0900

    [Silver IV] Title: 제로, Time: 224 ms, Memory: 24132 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -3,29 +3,24 @@ import java.io.*;
 
 public class Main{
     public static void main(String args[]) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        Deque<Integer> stack = new ArrayDeque<>();
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      StringBuilder sb = new StringBuilder();
+      int n = Integer.parseInt(br.readLine());
+      Stack<Integer> stack = new Stack<>();
+      for(int i = 0;i<n;i++){
         int k = Integer.parseInt(br.readLine());
-        for(int i=0;i<k;i++){
-            int n = Integer.parseInt(br.readLine());
-            if(n==0){
-                stack.pop();
-            }
-            else{
-                stack.push(n);
-            }
-        }
-        int sum = 0;
-        while(!stack.isEmpty()){
-            sum+=stack.pop();
-        }
-        sb.append(sum);
+        if(k!=0) stack.push(k);
+        else if(!stack.isEmpty()) stack.pop();
+      }
 
-
-        bw.write(sb.toString());
-        bw.flush();
+      int sum = 0;
+      while(!stack.isEmpty()){
+        sum+=stack.pop();
+      }
+      sb.append(sum);
+      bw.write(sb.toString());
+      bw.flush();
 
     }
 }
