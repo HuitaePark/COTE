b29699aeaf6b957317c17f721f71148c0e071d71
Baki <qkrgmlxo3174@gmail.com>
2026-01-01 23:17:55 +0900

    [Gold V] Title: 탑, Time: 720 ms, Memory: 86388 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,39 +1,38 @@
 import java.util.*;
 import java.io.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(br.readLine());
-        int[] towers = new int[n];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            towers[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && towers[stack.peek()] < towers[i]) {
-                stack.pop();
-            }
-
-            if (stack.isEmpty()) {
-                sb.append("0 ");
-            } else {
-                sb.append((stack.peek() + 1)).append(" ");
-            }
-
-            stack.push(i);
-        }
-
-        bw.write(sb.toString().trim());
-        bw.flush();
-        bw.close();
-        br.close();
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n+1];
+    int[] answer = new int[n+1];
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    Stack<Integer> stack = new Stack<>();
+    
+    for(int i= 1;i<=n;i++){
+      arr[i] = Integer.parseInt(st.nextToken());
     }
+
+    for(int i = n;i > 0; i--){
+      if(stack.isEmpty() || arr[stack.peek()]>arr[i]) {
+        stack.push(i);
+        continue;
+      }
+      while(!stack.isEmpty() && arr[stack.peek()]<arr[i]){
+        answer[stack.pop()] = i;
+      }
+      stack.push(i);
+    }
+
+    for(int i = 1; i <= n; i++){
+    sb.append(answer[i]).append(" ");
+    }
+    bw.write(sb.toString().trim());
+    bw.flush();
+  
+  }
 }
+
