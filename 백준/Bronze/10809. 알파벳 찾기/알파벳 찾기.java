52325dafc445bd7f9955cf061695a7465d4f8078
Baki <qkrgmlxo3174@gmail.com>
2026-01-02 10:11:53 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,29 +1,23 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] alphabet = new int[26];
-        String answer = sc.next();
-        
-        
-        for (int i = 0; i < alphabet.length; i++) {
-            alphabet[i] = -1;
-        }
-
-      
-        for (int i = 0; i < answer.length(); i++) {
-            char c = answer.charAt(i);
-            int index = c - 'a'; 
-            if (alphabet[index] == -1) {
-                alphabet[index] = i;
-            }
-        }
-
-
-        for (int i = 0; i < alphabet.length; i++) {
-            System.out.print(alphabet[i] + " ");
-        }
-        sc.close();
+import java.util.*;
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int[] arr = new int[26];
+    String s = br.readLine();
+    Arrays.fill(arr,-1);
+    
+    for(int i = 0;i<s.length();i++){
+      if(arr[s.charAt(i)-'a']==-1) arr[s.charAt(i)-'a'] = i;
     }
+    for(int i = 0;i<arr.length;i++){
+       sb.append(arr[i]).append(" ");
+    }
+    bw.write(sb.toString());
+    bw.flush();
+  
+  }
 }
