aef5179667719d851eb79cd10c703f05324813cc
Baki <qkrgmlxo3174@gmail.com>
2026-01-02 14:01:16 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 45796 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -8,25 +8,20 @@ public class Main{
     StringBuilder sb = new StringBuilder();
     int n = Integer.parseInt(br.readLine());
     Queue<Integer> q = new LinkedList<>();
+    for(int i = 1;i<=n;i++){
+      q.add(i);
+    }
+
+    while(q.size() > 1){
+      q.poll();
+      if(q.size()>1){
+        q.add(q.poll());
+      }
+    }
     
-    for(int i = 0;i<n;i++){
-      q.add(i+1);
-    }
-    int count = 1;
-    while(q.size()!=1){
-        int p = q.poll();
-        if(count%2==0){
-          q.add(p);
-        }
-      count++;
-    }
     sb.append(q.poll());
-
-
-
-
-     bw.write(sb.toString());
-     bw.flush();
+    bw.write(sb.toString());
+    bw.flush();
   
   }
 }
