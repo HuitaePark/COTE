cf1a78f86a9753606c799ee0baba1b992a87d5d9
Baki <qkrgmlxo3174@gmail.com>
2026-01-05 12:47:10 +0900

    [Bronze V] Title: 사분면 고르기, Time: 108 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -1,30 +1,23 @@
+// https://www.acmicpc.net/problem/14681
 import java.util.*;
-
+import java.io.*;
+  
 public class Main{
-    public static void main(String[] args){
-        
-        Scanner sc = new Scanner(System.in);
-        
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        
-        if(x>0 && y>0){
-            System.out.println(1);
-        }
-        else if(x<0 && y>0){
-            System.out.println(2);
-        }
-        else if(x<0 && y<0){
-            System.out.println(3);
-        }
-        else if(x>0 && y<0){
-            System.out.println(4);
-        }
-        
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int x  =Integer.parseInt(br.readLine());
+    int y = Integer.parseInt(br.readLine());
 
-        
-        
-        
-        sc.close();
-    }
+    if(x>0 && y>0) sb.append(1);
+    else if(x<0 && y<0) sb.append(3);
+    else if(x>0 && y<0) sb.append(4);
+    else if(x<0 && y>0) sb.append(2);
+
+
+    bw.write(sb.toString());
+    bw.flush();
+  
+  }
 }
