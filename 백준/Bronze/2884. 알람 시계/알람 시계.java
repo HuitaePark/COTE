d16202134a974078569eb3637750faea9878cff8
Baki <qkrgmlxo3174@gmail.com>
2026-01-06 15:57:12 +0900

    [Bronze III] Title: 알람 시계, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,21 +1,32 @@
 import java.util.*;
-
+import java.io.*;
+  
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int h = sc.nextInt();
-        int m = sc.nextInt();
-        if((m-45) < 0){
-            m = m + 15; 
-            if(h == 0){
-                h = 23;
-            } else {
-                h--;
-            }
-        } else {
-            m = m - 45;
-        }
-        System.out.println(h + " " + m);
-        sc.close();
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int h = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+
+
+    if(45<=m){
+      m -=45;
+      sb.append(h).append(" ").append(m);
     }
+    else{
+      m = (60 - (45 - m));
+      if(h==0){
+        h = 23;
+      }
+      else{
+        h--;
+      }
+      sb.append(h).append(" ").append(m);
+    }
+    bw.write(sb.toString());
+    bw.flush();
+  
+  }
 }
