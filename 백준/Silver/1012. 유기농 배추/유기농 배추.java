709d6ba55630809853b8e441005533baf283869c
Baki <qkrgmlxo3174@gmail.com>
2026-01-15 13:47:39 +0900

    [Silver II] Title: 유기농 배추, Time: 144 ms, Memory: 15840 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,64 +1,63 @@
 import java.util.*;
 import java.io.*;
 public class Main{
-
-    static int m;
-    static int n;
-    static int[][] arr;
-    static int[][] visited;
-
-    public static void main(String args[]) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        int num = Integer.parseInt(br.readLine());
-        
-        for(int i = 0; i < num; i++){
-            // 각 테스트 케이스마다 count를 초기화
-            int count = 0; 
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            m = Integer.parseInt(st.nextToken()); // m과 n을 정적 변수에 할당
-            n = Integer.parseInt(st.nextToken());
-            
-            arr = new int[m][n]; // 배열도 정적 변수에 할당
-            visited = new int[m][n];
-            
-            int k = Integer.parseInt(st.nextToken());
-            
-            // 배추가 심어져 있는곳 1로 바꾸기
-            for(int j = 0; j < k; j++){
-                st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-                arr[x][y] = 1;
-            }
-
-            for(int j = 0; j < m; j++){
-                for(int l = 0; l < n; l++){
-                    if(arr[j][l] == 1 && visited[j][l] == 0){
-                        count++;
-                        dfs(j, l); // dfs 호출
-                    }
-                }
-            }
-            sb.append(count).append("\n");
-        }
-        
-        bw.write(sb.toString());
-        bw.flush();
+  
+  static class Pair{
+    int x,y;
+    public Pair(int x, int y){
+      this.x = x;
+      this.y = y;
     }
-    
-    private static void dfs(int x, int y){
-        // 배열 범위 체크를 먼저 수행
-        if(x < 0 || x >= m || y < 0 || y >= n || arr[x][y] == 0 || visited[x][y] == 1){
-            return;
-        }
+  }  
+  
+  public static void main(String args[]) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      StringBuilder sb = new StringBuilder();
+      int[] dx = {1,0,-1,0};
+      int[] dy = {0,1,0,-1};
+      int t = Integer.parseInt(br.readLine());
+      for(int i =0;i<t;i++){
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] board = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
+        int count = 0;
+        Queue<Pair> q = new ArrayDeque<>();
         
-        visited[x][y] = 1;
+        for(int j = 0;j<k;j++){
+          st = new StringTokenizer(br.readLine());
+          int x = Integer.parseInt(st.nextToken());
+          int y = Integer.parseInt(st.nextToken());
+          board[x][y] = 1;
+        }
 
-        dfs(x + 1, y);
-        dfs(x - 1, y);
-        dfs(x, y + 1);
-        dfs(x, y - 1);
+        for(int j = 0;j<n;j++){
+          for(int l = 0;l<m;l++){
+            if(board[j][l]==1 && !visited[j][l]){
+             q.offer(new Pair(j,l));
+             visited[j][l] = true;
+             count++;
+              while(!q.isEmpty()){
+              Pair current = q.poll();
+              for(int dir = 0;dir<4;dir++){
+                int nx = current.x + dx[dir];
+                int ny = current.y + dy[dir];
+                if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
+                if(visited[nx][ny] || board[nx][ny]==0)continue;
+                visited[nx][ny] = true;
+                q.offer(new Pair(nx,ny));
+              }
+             }
+            }
+          }
+        }
+
+        sb.append(count).append("\n");
+      }
+      bw.write(sb.toString());
+      bw.flush();
     }
 }
