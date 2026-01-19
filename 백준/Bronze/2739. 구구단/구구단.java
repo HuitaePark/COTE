43ede0d53562993de6f8380a3042bd4f0ff857f5
Baki <qkrgmlxo3174@gmail.com>
2026-01-19 23:10:24 +0900

    [Bronze V] Title: 구구단, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,12 +1,18 @@
 import java.util.*;
-
+import java.io.*;
+  
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-        for(int i = 1;i<10;i++){
-            System.out.println(n+" * "+i+" = "+n*i);
-        }
-        sc.close();
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int n  =Integer.parseInt(br.readLine());
+
+    for(int i = 1;i<=9;i++){
+      sb.append(n).append(" * ").append(i).append(" = ").append(i*n).append("\n");
     }
+
+    bw.write(sb.toString());
+    bw.flush();
+  }
 }
