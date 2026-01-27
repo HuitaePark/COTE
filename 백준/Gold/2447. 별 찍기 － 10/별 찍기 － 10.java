5184675af076bb659b8838d6df2a694fc99981f8
Baki <qkrgmlxo3174@gmail.com>
2026-01-27 10:54:39 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 352 ms, Memory: 59020 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -2,60 +2,39 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-
-    static boolean[][] answer;
-    static int N; 
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  static String[][] arr;
+    
+  public static void main(String[] args) throws IOException {
         
-
-        N = Integer.parseInt(br.readLine());
-        
-
-        answer = new boolean[N][N];
-        
-
-        makeStar(0, 0, N);
-        
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (answer[i][j]) {
-                    sb.append('*');
-                } else {
-                    sb.append(' ');
-                }
-            }
-            sb.append('\n');
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));    
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+    arr = new String[n][n];
+    
+    func(0,0,n);
+        for(int i=0;i<n;i++){
+          for(int j=0;j<n;j++){
+            if(arr[i][j]==null) sb.append(" ");
+            else sb.append(arr[i][j]);
+          }
+          sb.append("\n");
         }
-        
-
         bw.write(sb.toString());
         bw.flush();
-        bw.close();
-        br.close();
-    }
-    
-    
-    static void makeStar(int r, int c, int n) {
 
-        if (n == 1) {
-            answer[r][c] = true;
-            return;
+    }
+    static void func(int r, int c,int n){
+      if(n==1){
+        arr[r][c] = "*";
+        return;
+      }
+      int size = n/3;
+      for(int i =0;i<3;i++){
+        for(int j =0;j<3;j++){
+          if(i==1&&j==1) continue;
+          func(r+i*size,c+j*size,size);
         }
-        
-        int size = n / 3;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (i == 1 && j == 1) {
-                    continue;
-                }
-                makeStar(r + i * size, c + j * size, size);
-            }
-        }
+      }
     }
 }
