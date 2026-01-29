4ca863e6fb43a827ab70d5d4020f3f1998126141
Baki <qkrgmlxo3174@gmail.com>
2026-01-30 01:49:50 +0900

    [level 1] Title: 카드 뭉치, Time: 0.09 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "";
+        Queue<String> q1 = new ArrayDeque<>();
+        Queue<String> q2 = new ArrayDeque<>();
+        for(int i =0;i<cards1.length;i++){
+            q1.offer(cards1[i]);
+        }
+        for(int i =0;i<cards2.length;i++){
+            q2.offer(cards2[i]);
+        }
+        for(int i =0;i<goal.length;i++){
+            if(!q1.isEmpty()&&q1.peek().equals(goal[i])) q1.poll();
+            else if(!q2.isEmpty()&&q2.peek().equals(goal[i])) q2.poll();
+            else return "No";
+        }
+        
+        return "Yes";
+    }
+}
