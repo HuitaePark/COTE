3a005bb84a8c31d04e2675a785c6acd89b0534c9
Baki <qkrgmlxo3174@gmail.com>
2026-01-31 01:45:01 +0900

    [level 1] Title: 2016년, Time: 1.43 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,10 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+class Solution {
+    public String solution(int a, int b) {
+        return LocalDate.of(2016,a,b)
+            .getDayOfWeek()
+            .name()
+            .substring(0,3);
+    }
+}
