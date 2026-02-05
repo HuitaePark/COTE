573d1e40f66cee77361ab44380c492166244e65b
Baki <qkrgmlxo3174@gmail.com>
2026-02-05 16:43:45 +0900

    [Silver V] Title: 색종이, Time: 92 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,29 +1,26 @@
 import java.util.*;
 import java.io.*;
-
-public class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        boolean[][] arr = new boolean[101][101];
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-
-            for (int j = y; j < y + 10; j++) {
-                for (int k = x; k < x + 10; k++) {
-                    if (!arr[j][k]) {
-                        arr[j][k] = true;
-                        sum++;
-                    }
-                }
-            }
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+    int[][] arr =new int[100][100];
+    int count = 0;
+    for(int k = 0;k<n;k++){
+      StringTokenizer st =new StringTokenizer(br.readLine());
+      int x = Integer.parseInt(st.nextToken());
+      int y = Integer.parseInt(st.nextToken());
+      for(int i = x;i<x+10;i++){
+        for(int j = y;j<y+10;j++){
+          if(++arr[i][j]==1) count++;
         }
-
-        System.out.println(sum);
+      }
     }
+    sb.append(count);  
+    bw.write(sb.toString());
+    bw.flush();
+  }
 }
