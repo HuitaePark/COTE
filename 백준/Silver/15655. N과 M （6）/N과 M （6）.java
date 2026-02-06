7cab64091067375058c3e3306dd86c29c390008a
Baki <qkrgmlxo3174@gmail.com>
2026-02-06 16:44:51 +0900

    [Silver III] Title: N과 M (6), Time: 100 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+  
+public class Main{
+  static int n,m;
+  static int[] arr;
+  static int[] temp = new int[40];
+  static StringBuilder sb = new StringBuilder();
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+    arr = new int[n];
+    st = new StringTokenizer(br.readLine());
+    for(int i=0;i<n;i++){
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+    Arrays.sort(arr);
+    func(0,0);
+    bw.write(sb.toString());
+    bw.flush();
+  }
+  static void func(int k, int start){
+    if(k==m){
+      for(int i = 0;i<m;i++){
+        sb.append(temp[i]).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
+    for(int i=start;i<n;i++){
+      temp[k] = arr[i];
+      func(k+1,i+1);
+    }
+  }
+}
