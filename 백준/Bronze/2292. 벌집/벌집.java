52c631894fd4434fbe7a2712eb94af83021c46fd
Baki <qkrgmlxo3174@gmail.com>
2026-02-16 22:34:31 +0900

    [Bronze II] Title: 벌집, Time: 112 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,36 +1,27 @@
-import java.io.*;
 import java.util.*;
-public class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int top = 7;
-        int bottom = 2;
-        int count = 2;
-        int i =0;
-        int n = Integer.parseInt(br.readLine());
-
-        if(n == 1){
-            bw.write(Integer.toString(n));
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine()); 
+    if(n==1) sb.append(1);
+    else {
+      n-=1;
+      int count = 1;
+      while(true){
+        if(n<=(count*6)) {
+          sb.append(count+1);
+          break;
         }
-        else if(2 <= n && n <= 7 ){
-            bw.write("2");
-        }
-        else{
-            while(true){
-                i +=6;
-                bottom +=i;
-                top += i+6;
-                count++;
-                if(bottom <= n && n <= top){
-                    bw.write(Integer.toString(count));
-                    break;
-                }
-            }
-        }
-        bw.flush();
+        n -= count * 6;
+        count++;
+      }
     }
 
+    bw.write(sb.toString());
+    bw.flush();
+  }
 }
-
