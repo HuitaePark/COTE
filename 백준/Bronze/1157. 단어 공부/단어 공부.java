d05fcb0c15dd14c13ffceb454f88baebf25bb81c
Baki <qkrgmlxo3174@gmail.com>
2026-02-16 22:44:25 +0900

    [Bronze I] Title: 단어 공부, Time: 256 ms, Memory: 21212 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,41 +1,35 @@
-import java.io.*;
 import java.util.*;
-
-
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        String str = st.nextToken();
-        str = str.toUpperCase();
-        int maxCount = 0;
-        int maxChar = 0;
-        int count = 0;
-        char[] arr = str.toCharArray();
-        int[] intArr = new int[26];
-        for (char i : arr){
-            intArr[i-'A']++;
-        }
-        for (int i = 0; i < 26; i++) {
-            if(intArr[i]>maxCount){
-                maxCount = intArr[i];
-                maxChar = i;
-            }
-        }
-        for (int i = 0; i < 26; i++) {
-            if(intArr[i]==maxCount){
-                count++;
-            }
-        }
-        char answer = (char) (maxChar+'A');
-        if(count>1){
-            System.out.println("?");
-        }
-        else{
-            System.out.println(answer);
-        }
-
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    String str = br.readLine().toLowerCase();
+    int[] arr = new int[26];
+    for(int i=0;i<str.length();i++){
+      arr[str.charAt(i)-'a']++;
     }
+    int max = Integer.MIN_VALUE;
+    int idx = 0;
+    for(int i=0;i<arr.length;i++){
+      if(max<arr[i]) {
+        idx = i;
+        max = arr[i];
+      }
+    }
+    boolean isDup = false;
+    for(int i=0;i<arr.length;i++){
+      if(i==idx) continue;
+      if(max==arr[i]){
+        isDup = true;
+      }
+    }
+    if(isDup) sb.append("?");
+    else sb.append((char)(idx+'a'));
+    
+    bw.write(sb.toString().toUpperCase());
+    bw.flush();
+  }
 }
