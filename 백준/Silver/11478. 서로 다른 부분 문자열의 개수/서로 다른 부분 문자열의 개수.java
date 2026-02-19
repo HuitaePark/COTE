6bf0a2bb35c121f34ae476eb262b2937f48eeecb
Baki <qkrgmlxo3174@gmail.com>
2026-02-20 01:28:57 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 572 ms, Memory: 226956 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
@@ -1,24 +1,22 @@
 import java.util.*;
 import java.io.*;
-  
-public class Main{
-  public static void main(String args[]) throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
-    StringBuilder sb = new StringBuilder();
-    String answer = br.readLine();
-    Set<String> set = new HashSet<>();
 
-    for(int i=1;i<=answer.length();i++){
-      for(int j=0;j<=answer.length()-i;j++){
-        String window = answer.substring(j,j+i);
-        set.add(window);
-      }
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        
+        Set<String> set = new HashSet<>();
+        
+        // i는 부분 문자열의 시작 인덱스
+        for (int i = 0; i < s.length(); i++) {
+            // j는 부분 문자열의 끝 인덱스 (i+1부터 문자열 끝까지)
+            for (int j = i + 1; j <= s.length(); j++) {
+                // substring을 이용해 문자열을 잘라서 바로 Set에 추가
+                set.add(s.substring(i, j));
+            }
+        }
+        
+        System.out.println(set.size());
     }
-
-    sb.append(set.size());    
-    bw.write(sb.toString());
-    bw.flush();
-  
-  }
 }
