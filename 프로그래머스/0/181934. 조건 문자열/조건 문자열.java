c5c858e19b286928f218e710e6d7bebc2d8ead9c
Baki <qkrgmlxo3174@gmail.com>
2026-02-21 17:19:22 +0900

    [level 0] Title: 조건 문자열, Time: 0.03 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        int answer = 0;
+        if(ineq.equals(">") && eq.equals("=")) return n>=m ? 1:0;
+        else if(ineq.equals("<") && eq.equals("=")) return n<=m ? 1:0;
+        else if(ineq.equals(">") && eq.equals("!")) return n>m ? 1:0;
+        else if(ineq.equals("<") && eq.equals("!")) return n<m ? 1:0;
+        return answer;
+    }
+}
