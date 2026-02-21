f19ec59e19607e28f41d6888a5af418b1482b48b
Baki <qkrgmlxo3174@gmail.com>
2026-02-22 01:45:52 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 544 ms, Memory: 58448 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -9,23 +9,18 @@ public class Main{
     StringTokenizer st = new StringTokenizer(br.readLine());
     int n = Integer.parseInt(st.nextToken());
     int m = Integer.parseInt(st.nextToken());
-
     Map<String,String> map = new HashMap<>();
-    for(int i = 0;i<n;i++){
+
+    for(int i=0;i<n;i++){
       st = new StringTokenizer(br.readLine());
       map.put(st.nextToken(),st.nextToken());
     }
 
-    for(int i = 0;i<m;i++){
+    for(int i=0;i<m;i++){
       sb.append(map.get(br.readLine())).append("\n");
     }
-
-
-
-
-
-     bw.write(sb.toString());
-     bw.flush();
+    bw.write(sb.toString());
+    bw.flush();
   
   }
 }
