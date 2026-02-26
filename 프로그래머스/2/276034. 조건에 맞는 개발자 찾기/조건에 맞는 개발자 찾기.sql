6e40a526175558cc7951736f9fabe4187c1454dd
Baki <qkrgmlxo3174@gmail.com>
2026-02-26 13:25:02 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,4 +1,9 @@
-SELECT ID,EMAIL,FIRST_NAME,LAST_NAME
-FROM DEVELOPERS
-WHERE (SKILL_CODE & 1280) !=0
-ORDER BY ID;
+SELECT d.ID,d.EMAIL,d.FIRST_NAME,d.LAST_NAME
+FROM DEVELOPERS d
+where exists(
+    select 1
+    from SKILLCODES s
+    where s.NAME in ('C#','Python')
+     AND (d.SKILL_CODE & s.CODE) != 0
+)
+ORDER BY d.ID asc;
