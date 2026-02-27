48c79902e955c1235d30412cbeae2dc0f748bf63
Baki <qkrgmlxo3174@gmail.com>
2026-02-28 01:16:49 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/README.md
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 조건에 부합하는 중고거래 상태 조회하기 - 164672 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164672?language=oracle) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164672?language=mysql) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2025년 08월 20일 18:42:02
+2026년 02월 28일 01:16:47
 
 ### 문제 설명
 
--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,13 +1,9 @@
-SELECT 
-    BOARD_ID,
-    WRITER_ID,
-    TITLE,
-    PRICE,
-    CASE STATUS
-        WHEN 'DONE' THEN '거래완료'
-        WHEN 'SALE' THEN '판매중'
-        WHEN 'RESERVED' THEN '예약중'
-    END AS STATUS
+SELECT BOARD_ID,WRITER_ID,TITLE,PRICE,
+CASE
+    when STATUS	='SALE' then '판매중'
+    when STATUS	='RESERVED' then '예약중'
+    when STATUS	='DONE' then '거래완료'
+    end as STATUS
 FROM USED_GOODS_BOARD
-WHERE TRUNC(CREATED_DATE) = TO_DATE('2022-10-05', 'YYYY-MM-DD')
-ORDER BY BOARD_ID DESC;
+WHERE DATE_FORMAT(CREATED_DATE,'%Y-%m-%d') = '2022-10-05'
+order by BOARD_ID desc; 
