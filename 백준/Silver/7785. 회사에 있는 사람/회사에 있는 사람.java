b675da29d48416004835487248980cc6cc6e1876
Baki <qkrgmlxo3174@gmail.com>
2026-02-28 12:33:08 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 752 ms, Memory: 58816 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -6,31 +6,22 @@ public class Main{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
     StringBuilder sb = new StringBuilder();
-    
     int n = Integer.parseInt(br.readLine());
-    Set<String> set = new HashSet<>();
-    
+    Set<String> set =new HashSet<>();
     for(int i=0;i<n;i++){
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      String person = st.nextToken();
-      String exit = st.nextToken();
-      if(!set.isEmpty()&&exit.equals("leave")){
-        set.remove(person);
-      }
-      else{
-        set.add(person);
-      }
+      String[] str = br.readLine().split(" ");
+      if(str[1].equals("enter")) set.add(str[0]);
+      else set.remove(str[0]);
     }
-    
-    String[] str = set.toArray(new String[0]);
-    Arrays.sort(str,Collections.reverseOrder());
-    for(String st : str){
-      sb.append(st+"\n");
+    List<String> list = new ArrayList<>();
+    for(String s: set){
+      list.add(s);
     }
-    
-
-     bw.write(sb.toString());
-     bw.flush();
-  
+    Collections.sort(list,Collections.reverseOrder());
+    for(String s : list){
+      sb.append(s).append("\n");
+    }
+    bw.write(sb.toString());
+    bw.flush();
   }
 }
