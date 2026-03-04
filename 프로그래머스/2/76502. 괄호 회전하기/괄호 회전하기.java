89c3bf9cc37e421185b96856c4effc3cc2fffb30
Baki <qkrgmlxo3174@gmail.com>
2026-03-04 16:04:35 +0900

    [level 2] Title: 괄호 회전하기, Time: 65.08 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,31 +1,63 @@
 import java.util.*;
 class Solution {
+    char[] original;
+    char[] copy;
+    int answer = 0;
     public int solution(String s) {
-        int answer = 0;
-        Map<Character,Character> map = new HashMap<>(); //판별용 맵 등록
-        map.put(')','(');
-        map.put('}','{');
-        map.put(']','[');
-        int num = s.length();
-        s+=s;//회전대신 두번 이어붙이기
-        A:for(int i = 0;i<num;i++){
-            ArrayDeque<Character> stack = new ArrayDeque<>();
-            for(int j=i;j<i+num;j++){
-                char c = s.charAt(j);
-                if(!map.containsKey(c)){ //열린 괄호일경우 푸쉬
-                    stack.push(c);
-                }
-                else{
-                    if(stack.isEmpty()|| !stack.pop().equals(map.get(c)))
-                        //닫힌 괄호인데 스택이 비어있거나 팝한 괄호가 닫힌괄호랑 동일할경우
-                        //내부 반복문은 종료하고 외부로 이동
-                        continue A;
-                }
-            }
-            if(stack.isEmpty())
-                answer++;
+        original = new char[s.length()];
+        for(int i=0;i<s.length();i++){
+            original[i] = s.charAt(i);
         }
-            
+        if(isCorrect(original)) answer++;
+        copy = rolling(original);
+        
+        while(!isEqual(original,copy)){
+            if(isCorrect(copy)) answer++;
+            copy = rolling(copy);
+        }
         return answer;
+    }
+    
+    char[] rolling(char[] arr){
+        char[] copy = new char[arr.length];
+        copy[copy.length-1] = arr[0];
+        for(int i=1;i<arr.length;i++){
+            copy[i-1] = arr[i];
+        }
+        return copy;
+    }
+    
+    boolean isEqual(char[] original,char[] copy){
+        for(int i=0;i<original.length;i++){
+            if(original[i]!=copy[i]) return false;
+        }
+        return true;
+    }
+    
+    boolean isCorrect(char[] arr){
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            if(stack.isEmpty()){
+                if(arr[i]==']' || arr[i]=='}' || arr[i]==')') return false;
+                else stack.push(arr[i]);
+            }
+            else if(stack.peek()=='['){
+                if(arr[i]==']') stack.pop();
+                else if(arr[i]=='}' || arr[i]==')') return false;
+                else stack.push(arr[i]);
+            }
+            else if(stack.peek()=='{'){
+                if(arr[i]=='}') stack.pop();
+                else if(arr[i]==']' || arr[i]==')') return false;
+                else stack.push(arr[i]);
+            }
+            else if(stack.peek()=='('){
+                if(arr[i]==')') stack.pop();
+                else if(arr[i]=='}' || arr[i]==']') return false;
+                else stack.push(arr[i]);
+            }
+        }
+        if(!stack.isEmpty()) return false;
+        return true;
     }
 }
