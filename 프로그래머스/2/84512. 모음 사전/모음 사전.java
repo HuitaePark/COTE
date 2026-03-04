9cd4710218103ee03d34e7a5b0995df27f8e611b
Baki <qkrgmlxo3174@gmail.com>
2026-03-04 15:35:49 +0900

    [level 2] Title: 모음 사전, Time: 15.75 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,25 +1,22 @@
 class Solution {
+    int count = 0;
+    int answer = 0;
+    char[] vowels = {'A', 'E', 'I', 'O', 'U'};
     public int solution(String word) {
-        int answer = 0;
-        char[] ch = {'A', 'E', 'I', 'O', 'U'};
-        
-        int[] weight = new int[5];
-        int val = 1;
-        for (int i = 4; i >= 0; i--) {
-            weight[i] = val;
-            val = val + (int) Math.pow(5, 5 - i); 
-        }
-        for (int i = 0; i < word.length(); i++) {
-            int idx = 0;
-            for (int j = 0; j < ch.length; j++) {
-                if (word.charAt(i) == ch[j]) {
-                    idx = j;
-                    break;
-                }
-            }
-            answer += (idx * weight[i]) + 1;
-        }
-        
+        dfs("",word);
         return answer;
+    }
+    void dfs(String current,String target){
+        if(current.equals(target)){
+            answer = count;
+            return;
+        }
+        if(current.length()==5) return;
+        
+        for(int i=0;i<5;i++){
+            count++;
+            dfs(current+vowels[i],target);
+            if(answer!=0) return;
+        }
     }
 }
