e5029dd36b7c9aad8ab10ace42c78104f20101cb
Baki <qkrgmlxo3174@gmail.com>
2026-03-05 17:11:28 +0900

    [level 2] Title: 땅따먹기, Time: 21.71 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,12 @@
+class Solution {
+    int solution(int[][] land) {
+        for(int i=1;i<land.length;i++){
+            land[i][0] += Math.max(land[i-1][3],Math.max(land[i-1][1],land[i-1][2]));
+            land[i][1] += Math.max(land[i-1][2],Math.max(land[i-1][3],land[i-1][0]));
+            land[i][2] += Math.max(land[i-1][1],Math.max(land[i-1][3],land[i-1][0]));
+            land[i][3] += Math.max(land[i-1][0],Math.max(land[i-1][1],land[i-1][2]));
+        }
+        int last = land.length-1;
+        return Math.max(Math.max(land[last][0],land[last][1]),Math.max(land[last][2],land[last][3]));
+    }
+}
