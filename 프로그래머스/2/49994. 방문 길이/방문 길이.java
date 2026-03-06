52d3d1bbde719938399b15a15cf6fddd2ea214af
Baki <qkrgmlxo3174@gmail.com>
2026-03-06 23:07:22 +0900

    [level 2] Title: 방문 길이, Time: 23.88 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,40 +1,52 @@
 import java.util.*;
 class Solution {
-    private static final HashMap<Character,int[]> location = new HashMap<>();
-    
     public int solution(String dirs) {
-        init();
-        int x = 5; int y = 5;
-        HashSet<String> answer = new HashSet<>();
-        
-        for(int i = 0;i<dirs.length();i++){
-            int[] offset = location.get(dirs.charAt(i));
-        
-            int nx = x+offset[0];
-            int ny = y+offset[1];
-            
-            if(!isMoveable(nx,ny)){
-                continue;
+        Set<String> set = new HashSet<>();
+        int x = 0;
+        int y = 0;
+        for(int i=0;i<dirs.length();i++){
+            char c = dirs.charAt(i);
+            int nx = 0;
+            int ny = 0;
+            switch(c){
+                case 'U':
+                    nx = x;
+                    ny = y+1;
+                    if(nx>5 || nx<-5 || ny>5 || ny<-5) continue;
+                    set.add(x+","+y+","+nx+","+ny);
+                    set.add(nx+","+ny+","+x+","+y);
+                    x = nx;
+                    y = ny;
+                    break;
+                case 'D':
+                    nx = x;
+                    ny = y-1;
+                    if(nx>5 || nx<-5 || ny>5 || ny<-5) continue;
+                    set.add(x+","+y+","+nx+","+ny);
+                    set.add(nx+","+ny+","+x+","+y);
+                    x = nx;
+                    y = ny;
+                    break;
+                case 'L':
+                    nx = x-1;
+                    ny = y;
+                    if(nx>5 || nx<-5 || ny>5 || ny<-5) continue;
+                    set.add(x+","+y+","+nx+","+ny);
+                    set.add(nx+","+ny+","+x+","+y);
+                    x = nx;
+                    y = ny;
+                    break;
+                case 'R':
+                    nx = x+1;
+                    ny = y;
+                    if(nx>5 || nx<-5 || ny>5 || ny<-5) continue;
+                    set.add(x+","+y+","+nx+","+ny);
+                    set.add(nx+","+ny+","+x+","+y);
+                    x = nx;
+                    y = ny;
+                    break;
             }
-            
-            answer.add(x +" " + y + " " + nx + " " + ny);
-            answer.add(nx +" " + ny + " " + x + " " + y);
-            
-            x = nx;
-            y = ny;
         }
-        
-        return answer.size()/2;
-    }
-    
-    private boolean isMoveable(int nx, int ny){
-        return 0 <= nx && nx < 11 && 0<= ny && ny < 11;
-    }
-    
-    private void init(){
-        location.put('U',new int[]{0,1});
-        location.put('D',new int[]{0,-1});
-        location.put('L',new int[]{-1,0});
-        location.put('R',new int[]{1,0});
+        return set.size()/2;
     }
 }
