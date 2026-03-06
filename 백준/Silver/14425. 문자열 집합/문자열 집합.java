e946998b0f0cb3fa4947ed5bd403d562228d6e3d
Baki <qkrgmlxo3174@gmail.com>
2026-03-06 21:48:48 +0900

    [Silver IV] Title: 문자열 집합, Time: 360 ms, Memory: 31200 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -6,24 +6,23 @@ public class Main{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
     StringBuilder sb = new StringBuilder();
+    Set<String> set = new HashSet<>();
     StringTokenizer st = new StringTokenizer(br.readLine());
     int n = Integer.parseInt(st.nextToken());
     int m = Integer.parseInt(st.nextToken());
-    int count = 0;
-    Set<String> set = new HashSet<>();
+
     for(int i=0;i<n;i++){
       set.add(br.readLine());
     }
+
+    int count = 0;
     for(int i=0;i<m;i++){
-      if(set.contains(br.readLine())){
-        count++;
-      }
+      if(set.contains(br.readLine())) count++;
     }
+
     sb.append(count);
-
-
-     bw.write(sb.toString());
-     bw.flush();
+    bw.write(sb.toString());
+    bw.flush();
   
   }
 }
