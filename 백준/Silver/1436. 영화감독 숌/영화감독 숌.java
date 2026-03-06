af216438b6eb09c2a99d69f22438e27515c68520
Baki <qkrgmlxo3174@gmail.com>
2026-03-06 19:14:26 +0900

    [Silver V] Title: 영화감독 숌, Time: 272 ms, Memory: 85700 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,22 +1,22 @@
+import java.util.*;
 import java.io.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        int cnt = 0;
-        int end = 666;
-        while(true){
-            String str = Integer.toString(end);
-            if(str.contains("666")){
-                cnt++;
-            }
-            if(cnt == n)
-                break;
-            end++;
-        }
-        bw.write(Integer.toString(end));
-        bw.flush();
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+    int count = 0;
+    int i = 1;
+    while(count!=n){
+      i++;
+      if(Integer.toString(i).contains("666")){
+        count++;
+      }
     }
+    sb.append(i);
+    bw.write(sb.toString());
+    bw.flush();
+  }
 }
