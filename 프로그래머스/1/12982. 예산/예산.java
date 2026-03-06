13148126ca6993002514fb689078c9dd6c2ce026
Baki <qkrgmlxo3174@gmail.com>
2026-03-06 17:00:28 +0900

    [level 1] Title: 예산, Time: 1.16 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -2,13 +2,11 @@ import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        int sum = 0;
         Arrays.sort(d);
-        for(int i =0;i<d.length;i++){
-            if(sum+d[i]<=budget){
-                sum+=d[i];
-                answer++;
-            }
+        int sum = 0;
+        for(int i=0;i<d.length;i++){
+            sum+=d[i];
+            if(sum<=budget) answer = i+1;
         }
         return answer;
     }
