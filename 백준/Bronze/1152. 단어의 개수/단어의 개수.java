969825a1ea201b3458ae6a51f76634b1727c6a44
Baki <qkrgmlxo3174@gmail.com>
2026-03-06 21:44:39 +0900

    [Bronze II] Title: 단어의 개수, Time: 376 ms, Memory: 53816 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,15 +1,15 @@
-import java.util.*;
 
+import java.util.*;
+import java.io.*;
+  
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String st = sc.nextLine().trim();
-       if (st.isEmpty()) {
-            System.out.println(0);
-        } else {
-            String[] words = st.split("\\s+");
-            System.out.println(words.length);
-        }
-        sc.close();
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    String s = br.readLine();
+    s = s.trim();
+    if (s.isEmpty()) System.out.println(0);
+    else System.out.println(s.split("\\s+").length);
     }
 }
