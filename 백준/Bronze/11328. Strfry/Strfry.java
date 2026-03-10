7a8f612f0d3ddc2e180780fc40eea25245823434
Baki <qkrgmlxo3174@gmail.com>
2026-03-10 17:10:16 +0900

    [Bronze II] Title: Strfry, Time: 280 ms, Memory: 20452 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.java
+++ 백준/Bronze/11328. Strfry/Strfry.java
@@ -1,39 +1,39 @@
 import java.util.*;
 import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+    
+    for(int i=0;i<n;i++){
+      int[] arr = new int[26];
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      String s = st.nextToken();
+      for(int j=0;j<s.length();j++){
+        arr[s.charAt(j)-'a']++;
+      }
 
-public class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String a = st.nextToken();
-            String b = st.nextToken();
-            int sum1 = 0;
-            int sum2 = 0;
-            boolean isStrfry = true;
-            if(a.length() != b.length()){
-              bw.write("Impossible" + "\n");
-              continue;
-            }
-            int[] arr =new int[26];
-            for(int j = 0; j<a.length();j++){
-              arr[a.charAt(j)-'a']++;
-              arr[b.charAt(j)-'a']--;
-            }
-            for(int j : arr){
-              if(j!=0){
-                isStrfry = false;
-                break;
-              }
-            }
-            
-            if (isStrfry)
-                bw.write("Possible" + "\n");
-            else
-                bw.write("Impossible" + "\n");
+      String s2 = st.nextToken();
+      for(int j=0;j<s2.length();j++){
+        arr[s2.charAt(j)-'a']--;
+      }
+
+      boolean isStrfry = true;
+      for(int j=0;j<arr.length;j++){
+        if(arr[j]!=0){
+          isStrfry = false;
+          break;
         }
-        bw.flush();
+      }
+
+      if(isStrfry) sb.append("Possible\n");
+      else sb.append("Impossible\n");
     }
+
+    bw.write(sb.toString());
+    bw.flush();
+  }
 }
