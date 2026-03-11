2116fcafd391956c6cb793267ab6918648186997
Baki <qkrgmlxo3174@gmail.com>
2026-03-12 08:58:45 +0900

    [Bronze V] Title: A+B - 3, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,14 +1,20 @@
 import java.util.*;
-
+import java.io.*;
+  
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();		
-        for(int i = 0;i<n;i++){
-			int a = sc.nextInt();
-            int b = sc.nextInt();
-            System.out.println(a+b);  
-		}
-        sc.close();
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+    for(int i=0;i<n;i++){
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      sb.append(a+b).append("\n");
     }
+
+    bw.write(sb.toString());
+    bw.flush();
+  }
 }
