1ff11d33cc73998b47321c6a807963a0658f3536
Baki <qkrgmlxo3174@gmail.com>
2026-03-12 09:00:22 +0900

    [Bronze V] Title: 합, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,15 +1,18 @@
-import java.io.*;
 import java.util.*;
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int sum = 0;
-        for(int i=1;i<=n;i++){
-            sum+=i;
-        }
-        System.out.print(sum);
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+    int sum = 0;
+    for(int i=1;i<=n;i++){
+      sum+=i;
     }
+    sb.append(sum);
+    bw.write(sb.toString());
+    bw.flush();
+  }
 }
