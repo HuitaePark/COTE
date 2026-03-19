b7f110e87af963243ebb3a4469f8acd4352e4ca2
Baki <qkrgmlxo3174@gmail.com>
2026-03-20 01:55:49 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 112 ms, Memory: 14768 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -1,16 +1,38 @@
+import java.util.*;
 import java.io.*;
+import java.math.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    BigInteger[] arr = new BigInteger[501];
+    arr[0] = BigInteger.ONE;
+    int n = Integer.parseInt(br.readLine());
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        
-        int count = 0;
-        while (n >= 5) {
-            count += n / 5;
-            n /= 5;
-        }
-        
-        System.out.println(count);
+    for(int i=1;i<arr.length;i++){
+      arr[i] = arr[i-1].multiply(BigInteger.valueOf(i+1));
     }
+    
+    if(n==0){
+      bw.write("0");
+      bw.flush();
+    }
+    else{
+    String s = arr[n-1].toString();
+    int idx = s.length()-1;
+    int count = 0;
+    for(int i = idx;i>=0;i--){
+      if(s.charAt(i)=='0') count++;
+      else break;
+    }
+
+    sb.append(count);
+    bw.write(sb.toString());
+    bw.flush();
+    }
+    br.close();
+    bw.close();
+  }
 }
