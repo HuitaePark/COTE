a392903aa0cc75aa375e6e04d79c970b76fef201
Baki <qkrgmlxo3174@gmail.com>
2026-03-20 18:08:01 +0900

    [Bronze I] Title: 이항 계수 1, Time: 100 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,28 +1,26 @@
-import java.io.*;
 import java.util.*;
-
-public class Main {
-    static int factorial(int n){
-
-        if(n == 1){
-            return 1;
-        }
-        else if(n == 0){
-            return 1;
-        }
-        else {
-            return n* factorial(n - 1);
-        }
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+    int[] arr = new int[11];
+    arr[1]=1;
+    for(int i=2;i<=10;i++){
+      arr[i] = arr[i-1] * i;
     }
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        long answer = factorial(N)/factorial(N-K)/factorial(K);
-
-        bw.write(Long.toString(answer));
-        bw.flush();
-    }
+    if(n==m ||m==0) sb.append(1);
+    else sb.append(
+      arr[n] / (arr[m] * arr[n-m])
+    );
+    
+    bw.write(sb.toString());
+    bw.flush();
+  
+  }
 }
