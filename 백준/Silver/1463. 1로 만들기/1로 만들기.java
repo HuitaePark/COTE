1ea25f39b28f22725ee11446b152a808393233fb
Baki <qkrgmlxo3174@gmail.com>
2026-03-23 15:28:43 +0900

    [Silver III] Title: 1로 만들기, Time: 132 ms, Memory: 18324 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -7,25 +7,18 @@ public class Main{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
     StringBuilder sb = new StringBuilder();
-    int N = Integer.parseInt(br.readLine());
+    int n = Integer.parseInt(br.readLine());
     int[] dp = new int[1000001];
-    
-    for(int i = 2;i<=N;i++){
+    dp[1] = 0;
+
+    for(int i=2;i<dp.length;i++){
       dp[i] = dp[i-1]+1;
-      if(i%2==0){
-       dp[i] =  Math.min(dp[i/2]+1,dp[i]);
-      }
-      if(i%3==0){
-       dp[i] =  Math.min(dp[i/3]+1,dp[i]);
-      }
+      if(i%2==0) dp[i] = Math.min(dp[i/2]+1,dp[i]);
+      if(i%3==0) dp[i] = Math.min(dp[i/3]+1,dp[i]);
     }
     
-    sb.append(dp[N]);
-
-
-
-     bw.write(sb.toString());
-     bw.flush();
-  
+    sb.append(dp[n]);
+    bw.write(sb.toString());
+    bw.flush();
   }
 }
