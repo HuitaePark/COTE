7f6ccc57e50a6410ce30bb19394280dd40dd9c8c
Baki <qkrgmlxo3174@gmail.com>
2026-03-23 19:46:52 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 660 ms, Memory: 59952 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -3,30 +3,33 @@ import java.io.*;
 
 public class Main {
     public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      StringBuilder sb = new StringBuilder();
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int n = Integer.parseInt(st.nextToken());
+      int m = Integer.parseInt(st.nextToken());
+      int[] arr = new int[n+1];
+      int[] dp = new int[n+1];
+      st = new StringTokenizer(br.readLine());
+      for(int i=1;i<=n;i++){
+        arr[i] = Integer.parseInt(st.nextToken());
+      }
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+      dp[1] = arr[1];
 
-        int[] arr = new int[n + 1]; // 0 ~ n
-
+      for(int i=2;i<=n;i++){
+        dp[i] = dp[i-1] + arr[i];
+      }
+      
+      for(int i=0;i<m;i++){
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            int x = Integer.parseInt(st.nextToken());
-            arr[i] = arr[i - 1] + x;
-        }
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            sb.append(arr[b] - arr[a - 1]).append("\n");
-        }
-
-        bw.write(sb.toString());
-        bw.flush();
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+        sb.append(dp[end]-dp[start-1]).append("\n");
+      }
+      
+      bw.write(sb.toString());
+      bw.flush();
     }
 }
