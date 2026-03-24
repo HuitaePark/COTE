36a333ea32b0640abf30b6df0bb80f61b5dd03c5
Baki <qkrgmlxo3174@gmail.com>
2026-03-24 17:25:34 +0900

    [Silver III] Title: 피보나치 함수, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -3,36 +3,33 @@ import java.io.*;
   
 public class Main{
 static Integer[][] dp = new Integer[41][2];
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      StringBuilder sb = new StringBuilder();
+      int[][] dp = new int[41][2];
+      //0은 0의 개수 1은 1의 개수
+      dp[0][0] = 1;
+      dp[0][1] = 0;
+      
+      dp[1][0] = 0;
+      dp[1][1] = 1;
 
-        // DP 테이블의 초기값 설정
-        dp[0][0] = 1; // N=0일 때, 0은 1번, 1은 0번
-        dp[0][1] = 0;
-        dp[1][0] = 0; // N=1일 때, 0은 0번, 1은 1번
-        dp[1][1] = 1;
-
-        int T = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine());
-            
-            for (int j = 2; j <= N; j++) {
-                if (dp[j][0] == null) {
-                    dp[j][0] = dp[j-1][0] + dp[j-2][0];
-                    dp[j][1] = dp[j-1][1] + dp[j-2][1];
-                }
-            }
-            sb.append(dp[N][0]).append(" ").append(dp[N][1]).append("\n");
-        }
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+      
+      for(int i=2;i<dp.length;i++){
+        dp[i][0] = dp[i-1][0] + dp[i-2][0];
+        dp[i][1] = dp[i-1][1] + dp[i-2][1];
+      }
+      
+      int t = Integer.parseInt(br.readLine());
+      for(int i=0;i<t;i++){
+        int current = Integer.parseInt(br.readLine());
+        sb.append(dp[current][0]).append(" ").append(dp[current][1]).append("\n");
+      }
+      bw.write(sb.toString());
+      bw.flush();
+      bw.close();
+      br.close();
     }
 }
 
