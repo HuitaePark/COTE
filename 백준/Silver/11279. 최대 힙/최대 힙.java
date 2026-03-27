93788b9c122b7c170c4ea00774a01cf5a34073aa
Baki <qkrgmlxo3174@gmail.com>
2026-03-27 23:50:41 +0900

    [Silver II] Title: 최대 힙, Time: 292 ms, Memory: 27028 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -7,24 +7,16 @@ public class Main{
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
     StringBuilder sb = new StringBuilder();
     int n = Integer.parseInt(br.readLine());
-    Queue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+    Queue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
     
-    for(int i = 0;i<n;i++){
-      int x = Integer.parseInt(br.readLine());
-      if(x==0){
-          if(heap.isEmpty()){
-          sb.append(0).append("\n");
-        }
-        else{
-          sb.append(heap.poll()).append("\n");
-        }
+    for(int i=0;i<n;i++){
+      int current = Integer.parseInt(br.readLine());
+      if(current==0){
+        if(q.isEmpty()) sb.append(0).append("\n");
+        else sb.append(q.poll()).append("\n");
       }
-      else{
-        heap.add(x);
-      }
-      
+      else q.offer(current); 
     }
-
     bw.write(sb.toString());
     bw.flush();
   
