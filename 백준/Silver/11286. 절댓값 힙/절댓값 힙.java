f5e52638f502f562d1a99efb276bb78610cf237c
Baki <qkrgmlxo3174@gmail.com>
2026-03-29 00:33:08 +0900

    [Silver I] Title: 절댓값 힙, Time: 284 ms, Memory: 25480 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -6,35 +6,18 @@ public class Main{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
     StringBuilder sb = new StringBuilder();
-    int n = Integer.parseInt(br.readLine());
-    PriorityQueue<Integer> heap = new PriorityQueue<>((a,b)->{
-      if(Math.abs(a) == Math.abs(b)){
-        return Integer.compare(a, b);
-      }
-      else{
-        return Integer.compare(Math.abs(a),Math.abs(b));
-      }
+    Queue<Integer> q = new PriorityQueue<>((a,b)->{
+      if(Math.abs(a)!=Math.abs(b)) return Math.abs(a)-Math.abs(b);
+      return a-b;
     });
-
-    for(int i = 0;i<n;i++){
-      int input = Integer.parseInt(br.readLine());
-      if(input==0){
-        if(heap.isEmpty()){
-          sb.append(0).append("\n");
-        }
-        else{
-          sb.append(heap.poll()).append("\n");
-        }
-      }
-      else{
-        heap.offer(input);
-      }
+    int n = Integer.parseInt(br.readLine());
+    for(int i=0;i<n;i++){
+      int current = Integer.parseInt(br.readLine());
+      if(current!=0) q.offer(current);
+      else if(!q.isEmpty() && current ==0) sb.append(q.poll()).append("\n");
+      else sb.append(0).append("\n");
     }
-
-
-
     bw.write(sb.toString());
     bw.flush();
-  
   }
 }
