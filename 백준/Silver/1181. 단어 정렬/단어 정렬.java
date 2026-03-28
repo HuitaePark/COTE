66096ff42664b6a42f236e4a08c3d3a9acbd8c9e
Baki <qkrgmlxo3174@gmail.com>
2026-03-28 12:27:15 +0900

    [Silver V] Title: 단어 정렬, Time: 340 ms, Memory: 24664 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,23 +1,31 @@
-import java.io.*;
 import java.util.*;
-
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        Set<String> set = new HashSet<>();
-
-        for (int i = 0; i < n; i++) {
-            set.add(br.readLine()); // 중복 자동 제거
-        }
-
-        String[] str = set.toArray(new String[0]);
-        Arrays.sort(str, Comparator
-                .comparingInt(String::length)
-                .thenComparing(Comparator.naturalOrder()));
-        for (String i : str){
-            System.out.println(i);
-        }
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    List<String> list = new ArrayList<>();
+    int n = Integer.parseInt(br.readLine());
+    Set<String> set = new HashSet<>();
+    for(int i=0;i<n;i++){
+      set.add(br.readLine());
     }
+    for(String s : set){
+      list.add(s);
+    }
+    Collections.sort(list,(a,b)->{
+      if(a.length()!=b.length()) return a.length()-b.length();
+      return a.compareTo(b);
+    });
+    
+    for(int i=0;i<list.size();i++){
+      sb.append(list.get(i)).append("\n");
+    }
+    bw.write(sb.toString());
+    bw.flush();
+    br.close();
+    bw.close();
+  }
 }
