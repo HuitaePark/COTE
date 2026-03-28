a9321de6d2bf556ee80ae586ef408417646913c0
Baki <qkrgmlxo3174@gmail.com>
2026-03-29 00:27:04 +0900

    [Silver V] Title: 다리 놓기, Time: 112 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -6,22 +6,31 @@ public class Main{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
     StringBuilder sb = new StringBuilder();
-    int n = Integer.parseInt(br.readLine());
-    for(int i = 0;i<n;i++){
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      int west = Integer.parseInt(st.nextToken());
-      int east = Integer.parseInt(st.nextToken());
-      long answer = 1;
-      for (int j = 0; j < west; j++) {
-          answer = answer * (east - j) / (j + 1);
-      }
-      sb.append(answer).append("\n");
+    int t = Integer.parseInt(br.readLine());
+    long[][] dp = new long[35][35];
+    
+    for (int i = 0; i < 35; i++) {
+            dp[i][0] = 1; // nC0 = 1
+            dp[i][i] = 1; // nCn = 1
     }
-
-
-
-     bw.write(sb.toString());
-     bw.flush();
-  
-  }
+    for (int i = 2; i < 35; i++) {
+      for (int j = 1; j < i; j++) {
+        dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+      }
+    }
+    for(int i=0;i<t;i++){
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int n = Integer.parseInt(st.nextToken());
+      int m = Integer.parseInt(st.nextToken());
+      if(n<m){
+        int tmp = 0;
+        tmp = n;
+        n = m;
+        m = tmp;
+      }
+      sb.append(dp[n][m]).append("\n");
+    }
+    bw.write(sb.toString());
+    bw.flush();
+    }
 }
