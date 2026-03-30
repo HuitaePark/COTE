d25556b59175858ad2b7538bec69dfcc1ddeac75
Baki <qkrgmlxo3174@gmail.com>
2026-03-30 19:17:26 +0900

    [Silver II] Title: 최소 힙, Time: 300 ms, Memory: 27336 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -7,26 +7,18 @@ public class Main{
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
     StringBuilder sb = new StringBuilder();
     int n = Integer.parseInt(br.readLine());
-    Queue<Integer> heap = new PriorityQueue<>();
-    
-    for(int i = 0;i<n;i++){
-      int x = Integer.parseInt(br.readLine());
-      if(x==0){
-          if(heap.isEmpty()){
-          sb.append(0).append("\n");
-        }
-        else{
-          sb.append(heap.poll()).append("\n");
-        }
+    Queue<Integer> q = new PriorityQueue<>();
+    for(int i=0;i<n;i++){
+      int current = Integer.parseInt(br.readLine());
+      if(current==0){
+        if(!q.isEmpty()) sb.append(q.poll()).append("\n");
+        else sb.append(0).append("\n");
       }
-      else{
-        heap.add(x);
+      else {
+        q.offer(current);
       }
-      
     }
-
     bw.write(sb.toString());
     bw.flush();
-  
   }
 }
