894ccda55a35d30dd782fe2b0cfafc31937f1814
Baki <qkrgmlxo3174@gmail.com>
2026-03-31 10:17:31 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 108 ms, Memory: 14416 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+    int[] dp = new int[1001];
+    dp[1] = 1;
+    dp[2] = 3;
+    
+    for(int i=3;i<1001;i++){
+      dp[i] = (dp[i-1] + dp[i-2]*2)%10007;
+    }
+    sb.append(dp[n]);
+    bw.write(sb.toString());
+    bw.flush();
+  
+  }
+}
