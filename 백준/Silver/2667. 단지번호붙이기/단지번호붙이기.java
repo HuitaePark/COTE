4c952541cbd69a3b6ea5f048cf5342b2e2f18b22
Baki <qkrgmlxo3174@gmail.com>
2026-04-03 11:28:41 +0900

    [Silver I] Title: 단지번호붙이기, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -5,60 +5,69 @@ import java.util.stream.Collectors;
 public class Main{
   
   static int n;
-  static String[] arr;
+  static char[][] board;
   static boolean[][] visited;
-  static Map<Integer,Integer> map;
-  static int count = 0;
+  static Queue<Pair> queue = new ArrayDeque<>();
+  static int[] dr = {1,-1,0,0};
+  static int[] dc = {0,0,1,-1};
   
   public static void main(String args[]) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
     StringBuilder sb = new StringBuilder();
     n = Integer.parseInt(br.readLine());
-    map = new HashMap<>();
+    board = new char[n][n];
     visited = new boolean[n][n];
-    arr = new String[n];
 
-    for(int i = 0;i<n;i++){
-      arr[i] = br.readLine();
+    for(int i=0;i<n;i++){
+      String s = br.readLine();
+      for(int j=0;j<n;j++){
+        board[i][j] = s.charAt(j);
+      }
     }
-    for(int i = 0;i<n;i++){
-      for(int j = 0;j<n;j++){
-        if(arr[i].charAt(j)=='1'&&!visited[i][j]){
-          count++;
-          map.put(count,0);
-          dfs(i,j,count);
+
+    Queue<Integer> q = new PriorityQueue<>();
+    for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+        if(board[i][j]=='1' && !visited[i][j]){
+          int count = bfs(i,j);
+          q.offer(count);
         }
       }
     }
-    
-    
-    sb.append(map.size()).append("\n");
-    List<Integer> answer = map.values().stream()
-    .sorted()
-    .collect(Collectors.toList());
 
-
-    for(Integer i : answer){
-      sb.append(i).append("\n");
+    sb.append(q.size()).append("\n");
+    while(!q.isEmpty()){
+      sb.append(q.poll()).append("\n");
     }
     
     bw.write(sb.toString());
     bw.flush();
-  
   }
-  private static void dfs(int x,int y,int count){
-    //종료조건
-    if(x<0||x>=n||y<0||y>=n||arr[x].charAt(y)=='0'||visited[x][y] == true){
-      return;
-    }
-
-    visited[x][y] = true;
-    map.put(count,map.getOrDefault(count,0)+1);
-    dfs(x + 1, y,count);
-    dfs(x - 1, y,count);
-    dfs(x, y + 1,count);
-    dfs(x, y - 1,count);
+  static int bfs(int i,int j){
+    int count = 1;
+    visited[i][j] = true;
+    queue.offer(new Pair(i,j));
     
+    while(!queue.isEmpty()){
+      Pair current = queue.poll();
+      for(int dir = 0;dir<4;dir++){
+        int nr = current.r + dr[dir];
+        int nc = current.c + dc[dir];
+        if(nr<0 || nr>=n || nc<0 || nc>=n) continue;
+        if(visited[nr][nc] || board[nr][nc]=='0') continue;
+        visited[nr][nc] = true;
+        queue.offer(new Pair(nr,nc));
+        count++;
+      }
+    }
+    return count;
+  }
+  static class Pair{
+    int r,c;
+    public Pair(int r, int c){
+      this.r = r;
+      this.c = c;
+    }
   }
 }
