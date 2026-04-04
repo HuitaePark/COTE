d7acb9b8c86ccd84e6188545a9d2d7d75519d9b3
Baki <qkrgmlxo3174@gmail.com>
2026-04-04 11:59:42 +0900

    [Silver V] Title: 너의 평점은, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -1,57 +1,62 @@
-import java.io.*;
 import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        String[] subject = new String[20];
-        double[] score = new double[20];
-        String[] grade = new String[20];
-        for (int i = 0; i < 20; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            subject[i] = st.nextToken();
-            score[i] = Double.parseDouble(st.nextToken());
-            grade[i] = st.nextToken();
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    double score = 0.0;
+    double sum = 0.0;
+    for(int i=0;i<20;i++){
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      String name = st.nextToken();
+      double d = Double.parseDouble(st.nextToken());
+      String rank = st.nextToken();
+      
+      if(rank.equals("P")){
+          continue;
+      }
+      
+      score+=d;
+    
+      switch(rank){
+        case "A+":
+          sum+=(d*4.5);
+          break;
+        case "A0":
+          sum+=(d*4.0);
+          break;
+        case "B+":
+          sum+=(d*3.5);
+          break;
+        case "B0":
+          sum+=(d*3.0);
+          break;
+        case "C+":
+          sum+=(d*2.5);
+          break;
+        case "C0":
+          sum+=(d*2.0);
+          break;
+        case "D+":
+          sum+=(d*1.5);
+          break;
+        case "D0":
+          sum+=(d*1.0);
+          break;
+        case "F":
+          sum+=(d*0.0);
+          break;
+        default:
+          continue;
         }
-        double sum = 0;
-        double scoreSum = 0;
-        for (int i = 0; i < 20; i++) {
-            double n = 0;
-            switch (grade[i]) {
-                case "A+":
-                    n = 4.5;
-                    break;
-                case "A0":
-                    n = 4.0;
-                    break;
-                case "B+":
-                    n = 3.5;
-                    break;
-                case "B0":
-                    n = 3.0;
-                    break;
-                case "C+":
-                    n = 2.5;
-                    break;
-                case "C0":
-                    n = 2.0;
-                    break;
-                case "D+":
-                    n = 1.5;
-                    break;
-                case "D0":
-                    n = 1.0;
-                    break;
-                case "F":
-                    n = 0.0;
-                    break;
-                case "P":
-                    continue;
-            }
-            scoreSum += score[i];
-            sum += score[i] * n;
-        }
-        System.out.println(sum / scoreSum);
-    }
+      }
+    
+    sb.append(sum/score);
+    bw.write(sb.toString());
+    bw.flush();
+    br.close();
+    bw.close();
+  }
 }
