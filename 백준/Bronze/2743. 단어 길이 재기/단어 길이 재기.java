1b91429c42e72d61a79c887de66a7989a8934ed6
Baki <qkrgmlxo3174@gmail.com>
2026-04-05 18:34:56 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 108 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -1,12 +1,16 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    sb.append(br.readLine().length());
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        String answer = st.nextToken();
-        System.out.println(answer.length());
-    }
+    bw.write(sb.toString());
+    bw.flush();
+    br.close();
+    bw.close();
+  }
 }
