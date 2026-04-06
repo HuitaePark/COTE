85912e9d3a112fc6924af669e2d780a12186aea7
Baki <qkrgmlxo3174@gmail.com>
2026-04-06 23:58:53 +0900

    [Bronze IV] Title: 영수증, Time: 108 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,24 +1,23 @@
-import java.io.*;
 import java.util.*;
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int x = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        int y = Integer.parseInt(st.nextToken());
-        String result = "No";
-        int sum = 0;
-        for (int i = 0; i < y; i++) {
-            st = new StringTokenizer(br.readLine());
-            int money = Integer.parseInt(st.nextToken());
-            int count = Integer.parseInt(st.nextToken());
-            sum += money *count;
-        }
-        if (sum == x){
-            result = "Yes";
-        }
-        System.out.println(result);
+import java.io.*;
+  
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    StringBuilder sb = new StringBuilder();
+    int money = Integer.parseInt(br.readLine());
+    int n = Integer.parseInt(br.readLine());
+    int sum = 0;
+    for(int i=0;i<n;i++){
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      sum+=(Integer.parseInt(st.nextToken())*Integer.parseInt(st.nextToken()));
     }
+    if(sum==money) sb.append("Yes");
+    else sb.append("No");
+    bw.write(sb.toString());
+    bw.flush();
+    br.close();
+    bw.close();
+  }
 }
