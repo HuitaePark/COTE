8da625e1a081a1048051c3dc2e65c657d028e509
Baki <qkrgmlxo3174@gmail.com>
2026-04-30 15:55:38 +0900

    [level 2] Title: H-Index, Time: 0.92 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,11 +1,10 @@
 import java.util.*;
 class Solution {
     public int solution(int[] citations) {
-        int size = citations.length;
         Arrays.sort(citations);
         for(int i=0;i<citations.length;i++){
             int current = citations[i];
-            if(current>=size-i) return size-i;
+            if(citations.length-i <= current) return citations.length-i;
         }
         return 0;
     }
