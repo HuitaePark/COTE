6e30c6da4b9d9d951bf2cc6cb3f4d0891b952c7f
Baki <qkrgmlxo3174@gmail.com>
2026-04-30 14:28:23 +0900

    [level 2] Title: 가장 큰 수, Time: 218.83 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,22 +1,14 @@
 import java.util.*;
-
+import java.util.stream.*;
 class Solution {
     public String solution(int[] numbers) {
-        List<String> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 0; i < numbers.length; i++){
-            list.add(String.valueOf(numbers[i]));
-        }
-
-        Collections.sort(list,(a,b)->(b+a).compareTo(a+b));
-
-        for(String s : list){
-            sb.append(s);
-        }
-
-        if(sb.charAt(0) == '0') return "0";
-
-        return sb.toString();
+        String answer = Arrays.stream(numbers)
+            .mapToObj(String::valueOf)
+            .sorted((a,b)->(b+a).compareTo(a+b))
+            .collect(Collectors.joining());
+        
+        if(answer.startsWith("0")) return "0";
+        
+        return answer;
     }
 }
