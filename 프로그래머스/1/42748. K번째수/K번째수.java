bb5fc0b3f267424da2357c052214c7ca6f029ec5
Baki <qkrgmlxo3174@gmail.com>
2026-04-30 14:24:05 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,13 +3,15 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         for(int i=0;i<commands.length;i++){
+            int start = commands[i][0]-1;
+            int end = commands[i][1]-1;
+            int target = commands[i][2];
             List<Integer> list = new ArrayList<>();
-            for(int j=commands[i][0]-1;j<commands[i][1];j++){
-                list.add(array[j]);
-            
+            for(int j=start;j<=end;j++){
+              list.add(array[j]);  
             }
             Collections.sort(list);
-            answer[i] = list.get(commands[i][2]-1);
+            answer[i] = list.get(target-1);
         }
         return answer;
     }
