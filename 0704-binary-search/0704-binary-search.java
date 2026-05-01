169273636bc4e3772f05f6e879c573f0225a0af9
Baki <qkrgmlxo3174@gmail.com>
2026-05-02 02:39:20 +0900

    Time: 4 ms (0.64%), Space: 48.7 MB (14.24%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int search(int[] nums, int target) {
+        Arrays.sort(nums);
+        int start = 0;
+        int end = nums.length-1;
+        
+
+        while(start<=end){
+            int mid = (start + end) / 2;
+
+            if(nums[mid]==target) return mid;
+
+            if(nums[mid]<target){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+
+        return -1;
+    }
+}
