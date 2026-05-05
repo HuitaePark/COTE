c4bf96fde968ece1aede52a44254de47fb1d61db
Baki <qkrgmlxo3174@gmail.com>
2026-05-05 22:04:11 +0900

    [level 2] Title: 전화번호 목록, Time: 270.52 ms, Memory: 218 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -5,14 +5,12 @@ class Solution {
         for(int i=0;i<phone_book.length;i++){
             set.add(phone_book[i]);
         }
-        for(int i=0;i<phone_book.length;i++){
-            String current = phone_book[i];
-            for(int j=1;j<current.length();j++){
-                String sub = current.substring(0,j);
-                if(set.contains(sub)) return false;
+        for(String s : phone_book){
+            for(int i=1;i<s.length();i++){
+                String current = s.substring(0,i);
+                if(set.contains(current)) return false;
             }
         }
-        
         return true;
     }
 }
