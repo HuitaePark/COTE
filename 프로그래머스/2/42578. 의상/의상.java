92b0b05f8f09db385dc9740815bacee0f28b94f7
Baki <qkrgmlxo3174@gmail.com>
2026-05-11 22:17:29 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,12 +3,13 @@ class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
         Map<String,Integer> map = new HashMap<>();
-        for(int i=0;i<clothes.length;i++){
-            map.put(clothes[i][1],map.getOrDefault(clothes[i][1],0)+1);
+        for(String[] s : clothes){
+            map.put(s[1],map.getOrDefault(s[1],0)+1);
         }
         for(String s : map.keySet()){
             answer*=(map.get(s)+1);
         }
-        return answer-1;
+        
+        return --answer;
     }
 }
