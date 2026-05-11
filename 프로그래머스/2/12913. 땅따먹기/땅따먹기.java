74ed4d093185377c231a9274cbf8577bf6f4066e
Baki <qkrgmlxo3174@gmail.com>
2026-05-12 03:06:20 +0900

    [level 2] Title: 땅따먹기, Time: 12.26 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,12 +1,26 @@
 class Solution {
     int solution(int[][] land) {
-        for(int i=1;i<land.length;i++){
-            land[i][0] += Math.max(land[i-1][3],Math.max(land[i-1][1],land[i-1][2]));
-            land[i][1] += Math.max(land[i-1][2],Math.max(land[i-1][3],land[i-1][0]));
-            land[i][2] += Math.max(land[i-1][1],Math.max(land[i-1][3],land[i-1][0]));
-            land[i][3] += Math.max(land[i-1][0],Math.max(land[i-1][1],land[i-1][2]));
+        int answer = 0;
+        int[][]dp = new int[land.length][4];
+        
+        for(int i=0;i<4;i++){
+            dp[0][i] = land[0][i];
         }
-        int last = land.length-1;
-        return Math.max(Math.max(land[last][0],land[last][1]),Math.max(land[last][2],land[last][3]));
+        
+        for(int i=1;i<dp.length;i++){
+            for(int j=0;j<4;j++){
+                int current = land[i][j];
+                for(int k=0;k<4;k++){
+                    if(k==j) continue;
+                    int cur = current + dp[i-1][k];
+                    dp[i][j] = Math.max(dp[i][j],cur);
+                }
+            }
+        }
+        
+        for(int i=0;i<4;i++){
+            answer = Math.max(answer,dp[land.length-1][i]);
+        }
+        return answer;
     }
 }
