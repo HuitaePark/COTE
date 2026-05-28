0a6bf67a9d2997d2d991ba6e472874ea24e2a04f
Baki <qkrgmlxo3174@gmail.com>
2026-05-28 19:35:34 +0900

    [Passed] 가장 왼쪽에 있는 최댓값 (127ms, 9MB)

---
+++ codetree/trail1/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }   
+
+        int end = n - 1;
+
+        while(true){
+            int max = Integer.MIN_VALUE;
+
+            for(int i = 0; i <= end; i++){
+                max = Math.max(max, arr[i]);
+            }
+
+            for(int i = 0; i <= end; i++){
+                if(arr[i] == max){
+                    sb.append(i + 1).append(" ");
+
+                    if(i == 0){
+                        System.out.print(sb);
+                        return;
+                    }
+
+                    end = i - 1;
+                    break;
+                }
+            }
+        }
+    }
+}
