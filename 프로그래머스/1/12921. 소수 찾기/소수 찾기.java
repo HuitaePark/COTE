1a2848ca70fba33d60d3f8761b6dc5da05e67ebb
Baki <qkrgmlxo3174@gmail.com>
2026-09-04 16:19:46 +0900

    [level 1] Title: 소수 찾기, Time: 37.79 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,18 +1,20 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for(int i=1;i<=n;i++){
+        for(int i =1;i<=n;i++){
             if(isPrime(i)) answer++;
         }
         return answer;
     }
     boolean isPrime(int n){
         if(n<=1) return false;
-        if(n==2) return true;
-        if(n%2==0) return false;
-        for(int i =3;i*i<=n;i+=2){
-            if(n%i==0) return false;
+        if(n==2 || n==3) return true;
+        if(n%2==0 || n%3==0) return false;
+        
+        for(int i=5;i*i<=n;i+=6){
+            if(n%i==0 || n%(i+2) == 0) return false;
         }
+        
         return true;
     }
 }
