ec8e37854bfdf5338d34e53cc0c295a3f61a22a4
Baki <qkrgmlxo3174@gmail.com>
2026-09-08 11:02:12 +0900

    [level 1] Title: 신고 결과 받기, Time: 147.99 ms, Memory: 191 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,38 +1,37 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        Map<String,Set<String>> map  = new HashMap<>();
-        Map<String,Integer> reported = new HashMap<>();
-        //맵 초기화
+        int[] answer = new int[id_list.length];
+        //유저 신고 내역
+        Map<String,Set<String>> map = new HashMap<>();
+        //신고당한 횟수 정산
+        Map<String,Integer> reported = new HashMap<>(); 
+        
         for(int i=0;i<id_list.length;i++){
-            map.put(id_list[i],new HashSet<>());
-            reported.put(id_list[i],0);
+            map.put(id_list[i],new HashSet<>());        
         }
-        //신고하기
+        
+        //신고 내역 저장
         for(int i=0;i<report.length;i++){
             String[] str = report[i].split(" ");
-            String current = str[0];
-            Set<String> currentSet = map.get(current);
-            currentSet.add(str[1]);
-            map.put(current,currentSet);
+            map.get(str[0]).add(str[1]);
         }
         
-        //신고당한 횟수 계산
-        for(Map.Entry<String,Set<String>> entry : map.entrySet()){
-            Set<String> currentSet = entry.getValue();
-            for(String s:currentSet){
-                reported.put(s,reported.get(s)+1);
-            }   
+        //신고횟수 세기
+        for(String s : map.keySet()){
+            for(String id : map.get(s)){
+                reported.put(id,reported.getOrDefault(id,0)+1);
+            }    
         }
         
-        int[] answer = new int[id_list.length];
+        //정산
         for(int i=0;i<answer.length;i++){
             String current = id_list[i];
-            Set<String> currentSet = map.get(current);
-            for(String s:currentSet){
+            for(String s : map.get(current)){
                 if(reported.get(s)>=k) answer[i]++;
             }
         }
-            return answer;
-        }
+        
+        return answer;
     }
+}
